65b2627b64a5da602682d5fd23b2fc9c8e612f5f
Agustina Pesce <pesce.agustina@gmail.com>
2018-10-31 16:22:18 -0300

    Modified the docstring

--- harmonica/isostasy.py
+++ harmonica/isostasy.py
@@ -3,38 +3,38 @@ import numpy as np
 def isostasy_airy(topography, density_crust, density_mantle, 
                   density_water=None):
     """
-        Computes the isostatic moho using the topography.
+    Computes the moho undulation using the Airy hypothesis.
 
-        On continental points:
+    On continental points:
 
-        .. math ::
-            moho_depth = \frac{\rho_c}{\rho_m - \rho_c} L
+    .. math ::
+        m = \frac{\rho_c}{\rho_m - \rho_c} h
 
-        On oceanic points:
+    On oceanic points:
+    
+    .. math ::
+        m = \frac{\rho_c - \rho_w}{\rho_m - \rho_c} h
+
+    where $L$ is the topography, $rho_m$ is the density of the mantle, 
+    $rho_w$ is the density of the water, $\rho_c$ is the crustal density.
+
+    Parameters:
+
+    * rho_m: float
+        Mantle density in kg/m$^3$.
+
+    * rho_c: float
+        Crustal density in kg/m$^3$.
         
-        .. math ::
-            moho_depth = \frac{\rho_c - \rho_w}{rho_m - rho_c} L
-
-        where $L$ is the topography, $rho_m$ is the density of the mantle, 
-        $rho_w$ is the density of the water, $\rho_c$ is the crustal density.
-
-        Parameters:
-
-        * rho_m: float
-            Mantle density in kg/m$^3$.
-
-        * rho_c: float
-            Crustal density in kg/m$^3$.
-            
-        * rho_w: float
-            Water density in kg/m$^3$.
-            
-        * L: float
-            Topography in meters.
-            
-        moho_depth:
+    * rho_w: float
+        Water density in kg/m$^3$.
         
-        * h: 1D-array
-            Isostatic moho in meters.
-        """
+    * h: float
+        Topography in meters.
+        
+    moho:
+    
+    * m: 1D-array
+        Isostatic moho undulation in meters.
+    """
       
