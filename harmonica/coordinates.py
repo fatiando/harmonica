f0ef2f365b9cee16bd4f78c8e034f79959d156e2
Santiago Soler <santiago.r.soler@gmail.com>
2018-11-08 17:00:30 -0300

    Add geodetic to geocentric coords transformation

---
+++ harmonica/coordinates.py
@@ -0,0 +1,35 @@
+"""
+Geographic coordinate conversion.
+"""
+import numpy as np
+
+from . import get_ellipsoid
+
+
+def geodetic_to_geocentric(latitude, height):
+    """
+    Convert from geodetic to geocentric coordinates.
+
+    Parameters
+    ----------
+    latitude : float or array
+        The geodetic latitude (in degrees).
+    height : float or array
+        The ellipsoidal (geometric or geodetic) height.
+
+    Returns
+    -------
+    """
+    ellipsoid = get_ellipsoid()
+    # Convert latitude to radians
+    latitude_rad = np.pi / 180 * latitude
+    prime_vertical_radius = ellipsoid.semimajor_axis / np.sqrt(
+        1 - ellipsoid.linear_eccetricity ** 2 * np.sin(latitude_rad) ** 2
+    )
+    xy_projection = (height + prime_vertical_radius) * np.cos(latitude_rad)
+    z_cartesian = (
+        height + (1 - ellipsoid.linear_eccentricity ** 2) * prime_vertical_radius
+    ) * np.sin(latitude_rad)
+    radius = np.sqrt(xy_projection ** 2 + z_cartesian ** 2)
+    geocentric_latitude = np.arcsin(z_cartesian / radius)
+    return geocentric_latitude, radius
