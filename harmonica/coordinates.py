2779e4a4a7fdb8967f06828ada4ff99c84c38a9b
Santiago Soler <santiago.r.soler@gmail.com>
2018-11-09 11:27:46 -0300

    Improve geodetic to geocentric docstring

--- harmonica/coordinates.py
+++ harmonica/coordinates.py
@@ -10,15 +10,25 @@ def geodetic_to_geocentric(latitude, height):
     """
     Convert from geodetic to geocentric coordinates.
 
+    The geodetic datum is defined by the default :class:`harmonica.ReferenceEllipsoid`
+    set by the :func:`harmonica.set_ellipsoid` function.
+    The coordinates are converted following [Vermeille2002]_.
+
     Parameters
     ----------
     latitude : float or array
         The geodetic latitude (in degrees).
     height : float or array
-        The ellipsoidal (geometric or geodetic) height.
+        The ellipsoidal (geometric or geodetic) height (in meters).
 
     Returns
     -------
+    geocentric_latitude : float or array
+        The geocentric latitude (in degrees).
+    radius : float or array
+        The radial distance in the geocentric coordinate system (in meters), i.e.  the
+        distance between the point given by ``latitude`` and ``height`` and the origin
+        of the geocentric reference system.
     """
     ellipsoid = get_ellipsoid()
     # Convert latitude to radians
@@ -26,6 +36,8 @@ def geodetic_to_geocentric(latitude, height):
     prime_vertical_radius = ellipsoid.semimajor_axis / np.sqrt(
         1 - ellipsoid.linear_eccentricity ** 2 * np.sin(latitude_rad) ** 2
     )
+    # Instead of computing X and Y, we only comupute the projection on the XY plane:
+    # xy_projection = sqrt( X**2 + Y**2 )
     xy_projection = (height + prime_vertical_radius) * np.cos(latitude_rad)
     z_cartesian = (
         height + (1 - ellipsoid.linear_eccentricity ** 2) * prime_vertical_radius
