2421a257be44b64dba497baa1c00a116b66ae9be
Leonardo Uieda <leouieda@gmail.com>
2018-12-21 19:29:39 -1000

    Trying a simpler rewrite (not done)

--- harmonica/forward/prism.py
+++ harmonica/forward/prism.py
@@ -14,18 +14,20 @@ def prism_gravity(coordinates, prism, density, components, dtype="float64"):
     """
     """
     easting, northing, vertical = (i.ravel() for i in coordinates[:3])
+    # Initialize with zeros
+    cast = np.broadcast(*coordinates[:3])
+    result = np.zeros((len(components), cast.size), dtype=dtype)
+
     west, east, south, north, top, bottom = prism
     # First thing to do is make the computation point the origin of the coordinate
     # system
     x = [north - northing, south - northing]
     y = [east - easting, west - easting]
     z = [bottom - vertical, top - vertical]
-    # Initialize with zeros
-    cast = np.broadcast(*coordinates[:3])
-    result = np.zeros((len(components), cast.size), dtype=dtype)
     # Evaluate the integration limits
     for i, j, k in itertools.product(range(2), range(2), range(2)):
-        prism_kernels(x[i], y[j], z[k], components, (-1)**(i + j + k),  result)
+        for c in range(result.shape[0]):
+            kernel_v(x[i], y[j], z[k], (-1)**(i + j + k), result[c])
     # Now all that is left is to multiply result by the gravitational constant and
     # convert it to mGal units
     result *= GRAVITATIONAL_CONST*1e5*density
@@ -34,15 +36,34 @@ def prism_gravity(coordinates, prism, density, components, dtype="float64"):
         return result[0]
     return result
 
+@jit(nopython=True, fastmath=True)
+def _prism_gravity(east, north, vertical, prism, kernel, out):
+    for l in range(output.size):
+        for i in range(2):
+            for j in range(2):
+                for k in range(2):
+                    out[l] += (-1)**(i + j + k)*kernel(east[i], north
 
-# @jit(nopython=True)
-@vectorize
+
+@jit(nopython=True, fastmath=True)
+def kernel_v(x, y, z, scale, output):
+    for i in range(output.size):
+        r = np.sqrt(x[i]**2 + y[i]**2 + z[i]**2)
+        # Minus because Nagy et al (2000) give the formula for the
+        # gradient of the potential and gravity is -grad(V)
+        kernel = -(x[i]*log(y[i] + r) +
+                   y[i]*log(x[i] + r) -
+                   z[i]*atan2(x[i]*y[i], z[i]*r))
+        output[i] += scale*kernel
+
+
+@jit
 def atan2(y, x):
     """
     Correct the value of the angle returned by arctan2 to match the sign of the
     tangent. Also return 0 instead of 2Pi for 0 tangent.
     """
-    result = math.atan2(y, x)
+    result = np.arctan2(y, x)
     if y > 0 and x < 0:
         result -= np.pi
     elif y < 0 and x < 0:
@@ -50,33 +71,17 @@ def atan2(y, x):
     return result
 
 
-# @jit(nopython=True)
-@vectorize
+@jit
 def log(x):
     """
     Return 0 for log(0) because the limits in the formula terms tend to 0
     (see Nagy et al., 2000)
     """
-    if math.abs(x) < 1e-10:
+    if np.abs(x) < 1e-10:
         result = 0
     else:
-        result = math.log(x)
+        result = np.log(x)
     return result
-
-
-@jit(nopython=True, fastmath=True)
-def prism_kernels(x, y, z, components, scale, output):
-    for i in range(output.shape[1]):
-        r = np.sqrt(x[i]**2 + y[i]**2 + z[i]**2)
-        for j in range(output.shape[0]):
-            if components[j] == "v":
-                # Minus because Nagy et al (2000) give the formula for the
-                # gradient of the potential and gravity is -grad(V)
-                kernel = -(x[i]*log(y[i] + r) +
-                           y[i]*log(x[i] + r) -
-                           z[i]*atan2(x[i]*y[i], z[i]*r))
-            output[j, i] += scale*kernel
-    return output
 
 def kernel_nn(x, y, z, r):
     kernel = -safe_atan2(z*y, x*r)
