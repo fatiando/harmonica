b5b4af93d512cd79e0d1fdac4231fca1e80fce0d
Santiago Soler <santiago.r.soler@gmail.com>
2019-05-20 16:06:48 -0300

    Add missing space in docstring

--- harmonica/forward/tesseroid.py
+++ harmonica/forward/tesseroid.py
@@ -31,7 +31,7 @@ def tesseroid_gravity(
 
     Parameters
     ----------
-    coordinates: list or 1d-array
+    coordinates : list or 1d-array
         List or array containing ``longitude``, ``latitude`` and ``radius`` of a single
         computation point defined on a spherical geocentric coordinate system.
         Both ``longitude`` and ``latitude`` should be in degrees and ``radius`` in meters.
