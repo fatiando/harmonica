c714a858fcdeeb3b3527eb2b1df5616b53e61389
Santiago Soler <santiago.r.soler@gmail.com>
2019-06-28 17:13:53 -0300

    Start experimenting with EQL

---
+++ harmonica/equivalent_layer/harmonic.py
@@ -0,0 +1,43 @@
+"""
+2D Equivalent Layer Gridders for harmonic functions
+"""
+import numpy as np
+from verde.base import BaseGridder, check_is_fitted, check_fit_input, least_squares
+
+
+class HarmonicEQL(BaseGridder):
+    """
+    2D Equivalent Layer Gridder for harmonic fields using Green's functions
+    """
+
+    def __init__(self):
+        """
+        """
+
+    def fit(self, coordinates, data, weights=None):
+        """
+        """
+        coordinates, data, weights = check_fit_input(coordinates, data, weights)
+
+    def predict(self, coordinates):
+        """
+        """
+        # We know the gridder has been fitted if it has the mean
+        check_is_fitted(self, ["mean_"])
+
+    def jacobian(self, coordinates, point_masses):
+        """
+        """
+
+
+def greens_func(coordinates, point_masses):
+    """
+    """
+    east, north, vertical = coordinates[:3]
+    east_p, north_p, vertical_p = point_masses[:]
+    distance = np.sqrt(
+        (east - east_p) ** 2
+        + (north - north_p) ** 2
+        + (vertical - vertical_p) ** 2
+    )
+    return 1 / distance
