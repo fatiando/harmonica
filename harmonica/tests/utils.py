bb6bb606c207921c480923d90c9796a0cc57b34d
Santiago Soler <santiago.r.soler@gmail.com>
2019-07-17 11:55:47 -0300

    Remove environmental error on require_numba
    If the env variable NUMBA_DISABLE_JIT is not defined, we will assume
    Numba jit is enabled (as Numba does).

--- harmonica/tests/utils.py
+++ harmonica/tests/utils.py
@@ -9,6 +9,9 @@ def require_numba(function):  # pylint: disable=unused-argument
     """
     Decorator to tell pytest to run the test function only if Numba jit is enabled.
 
+    To disable Numba jit the environmental variable ```NUMBA_DISABLE_JIT``` must be set
+    to a value different than 0.
+
     Use this decorator on test functions that involve great computational load and don't
     want to run if Numba jit is disabled.
     The decorated test functions will be run and checked if pass or fail, but won't be
@@ -18,16 +21,9 @@ def require_numba(function):  # pylint: disable=unused-argument
     function will be run twice: one with Numba jit enabled, and another one with Numba
     jit disable to check coverage.
     """
-    # Check if env variable NUMBA_DISABLE_JIT is defined
-    if os.getenv("NUMBA_DISABLE_JIT") is None:
-        raise EnvironmentError(
-            "Enviromental variable NUMBA_DISABLE_JIT is not defined."
-            + " Cannot run tests unless it's set to 0 or 1"
-            + " (for running non-compiled or compiled versios of jitted functions,"
-            + " respectively)."
-        )
     # Check if Numba is disabled
-    numba_is_disabled = bool(os.getenv("NUMBA_DISABLE_JIT") != "0")
+    # (if NUMBA_DISABLE_JIT is not defined, we will assume Numba jit is enabled)
+    numba_is_disabled = bool(os.environ.get("NUMBA_DISABLE_JIT", default="0") != "0")
 
     @pytest.mark.use_numba
     @pytest.mark.skipif(numba_is_disabled, reason="Numba jit is disabled")
