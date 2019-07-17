57b8ed19a4e350696f02e3406353614cc6cf7204
Leonardo Uieda <leouieda@gmail.com>
2019-07-17 11:59:42 -1000

    Format using black

--- examples/forward/tesseroid.py
+++ examples/forward/tesseroid.py
@@ -23,8 +23,11 @@ tesseroid = [-70, -50, -40, -20, ellipsoid.mean_radius - 10e3, ellipsoid.mean_ra
 density = 2670
 
 # Define computation points on a regular grid at 100km above the mean Earth radius
-coordinates = vd.grid_coordinates(region=[-80, -40, -50, -10], shape=(80, 80),
-                                  extra_coords=100e3 + ellipsoid.mean_radius)
+coordinates = vd.grid_coordinates(
+    region=[-80, -40, -50, -10],
+    shape=(80, 80),
+    extra_coords=100e3 + ellipsoid.mean_radius,
+)
 
 # Compute the radial component of the acceleration
 gravity = hm.tesseroid_gravity(coordinates, tesseroid, density, field="g_r")
@@ -34,9 +37,7 @@ print(gravity)
 fig = plt.figure(figsize=(8, 9))
 ax = plt.axes(projection=ccrs.Orthographic(central_longitude=-60))
 img = ax.pcolormesh(*coordinates[:2], gravity, transform=ccrs.PlateCarree())
-plt.colorbar(
-    img, ax=ax, pad=0, aspect=50, orientation="horizontal", label="mGal",
-)
+plt.colorbar(img, ax=ax, pad=0, aspect=50, orientation="horizontal", label="mGal")
 ax.coastlines()
 ax.set_title("Radial component of gravitational acceleration")
 plt.show()
