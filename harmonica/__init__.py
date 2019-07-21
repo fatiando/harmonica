4293aa120202064f262f580e0abcbcb07e864b77
Santiago Soler <santiago.r.soler@gmail.com>
2019-07-21 10:29:21 -0300

    Import EQLHarmonic on harmonica/__init__.py

--- harmonica/__init__.py
+++ harmonica/__init__.py
@@ -12,6 +12,7 @@ from .io import load_icgem_gdf
 from .isostasy import isostasy_airy
 from .gravity_corrections import normal_gravity, bouguer_correction
 from .coordinates import geodetic_to_spherical, spherical_to_geodetic
+from .equivalent_layer.harmonic import EQLHarmonic
 
 
 # Get the version number through versioneer
