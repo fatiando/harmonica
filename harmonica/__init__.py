cd3ff72163f6469035aa3cebf6cf8a1be7e4dc84
Santiago Soler <santiago.r.soler@gmail.com>
2019-07-30 10:13:07 -0300

    Remove distance function from the API

--- harmonica/__init__.py
+++ harmonica/__init__.py
@@ -14,7 +14,6 @@ from .gravity_corrections import normal_gravity, bouguer_correction
 from .coordinates import geodetic_to_spherical, spherical_to_geodetic
 from .forward.point_mass import point_mass_gravity
 from .forward.tesseroid import tesseroid_gravity
-from .forward.utils import distance
 
 
 # Get the version number through versioneer
