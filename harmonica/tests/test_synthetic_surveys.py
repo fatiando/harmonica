470c24c429a34f4f33cf8ae3fbd4d186195b4e23
Santiago Soler <santiago.r.soler@gmail.com>
2019-11-07 10:30:45 -0300

    Update test functions for synthetic surveys

--- harmonica/tests/test_synthetic_surveys.py
+++ harmonica/tests/test_synthetic_surveys.py
@@ -9,61 +9,86 @@ def test_ground_survey():
     """
     Test if the sythetic ground survey returns the expected survey
     """
-    region = (-1.2e3, 4.3e3, -6.4e3, -1.3e3)
-    survey = ground_survey(region)
-    assert set(survey.columns) == set(["easting", "northing", "elevation"])
-    assert survey.easting.size == 963
-    npt.assert_allclose(survey.easting.min(), region[0])
-    npt.assert_allclose(survey.easting.max(), region[1])
-    npt.assert_allclose(survey.northing.min(), region[2])
-    npt.assert_allclose(survey.northing.max(), region[3])
-    npt.assert_allclose(survey.elevation.min(), 0.0)
-    npt.assert_allclose(survey.elevation.max(), 2052.2)
+    # Ground survey without scaling
+    subsection = (13.60, 20.30, -24.20, -17.5)  # this is the default subsection
+    expected_region = (13.60833, 20.28333, -24.2, -17.50333)
+    survey = ground_survey()
+    assert set(survey.columns) == set(["longitude", "latitude", "height"])
+    assert survey.longitude.size == 963
+    npt.assert_allclose(survey.longitude.min(), expected_region[0])
+    npt.assert_allclose(survey.longitude.max(), expected_region[1])
+    npt.assert_allclose(survey.latitude.min(), expected_region[2])
+    npt.assert_allclose(survey.latitude.max(), expected_region[3])
+    npt.assert_allclose(survey.height.min(), 0.0)
+    npt.assert_allclose(survey.height.max(), 2052.2)
+    # Ground survey with scaling
+    region = (-10.1, 9.7, -20.3, -10.5)  # a random region to scale the survey
+    survey = ground_survey(region=region)
+    assert set(survey.columns) == set(["longitude", "latitude", "height"])
+    assert survey.longitude.size == 963
+    npt.assert_allclose(survey.longitude.min(), region[0])
+    npt.assert_allclose(survey.longitude.max(), region[1])
+    npt.assert_allclose(survey.latitude.min(), region[2])
+    npt.assert_allclose(survey.latitude.max(), region[3])
+    npt.assert_allclose(survey.height.min(), 0.0)
+    npt.assert_allclose(survey.height.max(), 2052.2)
 
 
 def test_airborne_survey():
     """
     Test if the synthetic airborne survey returns the expected survey
     """
-    region = (-1.2e3, 4.3e3, -6.4e3, -1.3e3)
-    survey = airborne_survey(region)
-    assert set(survey.columns) == set(["easting", "northing", "elevation"])
-    assert survey.easting.size == 5673
-    npt.assert_allclose(survey.easting.min(), region[0])
-    npt.assert_allclose(survey.easting.max(), region[1])
-    npt.assert_allclose(survey.northing.min(), region[2])
-    npt.assert_allclose(survey.northing.max(), region[3])
-    npt.assert_allclose(survey.elevation.min(), 359.0)
-    npt.assert_allclose(survey.elevation.max(), 1255.0)
+    subsection = (-5.0, -4.0, 56.0, 56.5)  # this is the default subsection
+    expected_region = (-4.99975, -4.00003, 56.00011, 56.49997)
+    survey = airborne_survey()
+    assert set(survey.columns) == set(["longitude", "latitude", "height"])
+    assert survey.longitude.size == 5673
+    npt.assert_allclose(survey.longitude.min(), expected_region[0])
+    npt.assert_allclose(survey.longitude.max(), expected_region[1])
+    npt.assert_allclose(survey.latitude.min(), expected_region[2])
+    npt.assert_allclose(survey.latitude.max(), expected_region[3])
+    npt.assert_allclose(survey.height.min(), 359.0)
+    npt.assert_allclose(survey.height.max(), 1255.0)
+    # Ground survey with scaling
+    region = (-10.1, 9.7, -20.3, -10.5)  # a random region to scale the survey
+    survey = airborne_survey(region=region)
+    assert set(survey.columns) == set(["longitude", "latitude", "height"])
+    assert survey.longitude.size == 5673
+    npt.assert_allclose(survey.longitude.min(), region[0])
+    npt.assert_allclose(survey.longitude.max(), region[1])
+    npt.assert_allclose(survey.latitude.min(), region[2])
+    npt.assert_allclose(survey.latitude.max(), region[3])
+    npt.assert_allclose(survey.height.min(), 359.0)
+    npt.assert_allclose(survey.height.max(), 1255.0)
 
 
-def test_cut_region_ground_survey():
+def test_subsection_ground_survey():
     """
-    Test if a different cut_region produces a different ground survey
+    Test if ground survey is changed against a different subsection
     """
-    region = (-50e3, 50e3, -50e3, 50e3)
-    cut_region = (10, 30, -30, -12)
-    survey = ground_survey(region, cut_region=cut_region)
-    assert survey.easting.size > 963
-    npt.assert_allclose(survey.easting.min(), region[0])
-    npt.assert_allclose(survey.easting.max(), region[1])
-    npt.assert_allclose(survey.northing.min(), region[2])
-    npt.assert_allclose(survey.northing.max(), region[3])
-    assert survey.elevation.min() <= 0.0
-    assert survey.elevation.max() >= 2052.2
+    subsection = (10, 30, -30, -12)  # a bigger subsection than the default one
+    region = (-10.1, 9.7, -20.3, -10.5)  # a random region to scale the survey
+    survey = ground_survey(region=region, subsection=subsection)
+    assert survey.longitude.size > 963
+    npt.assert_allclose(survey.longitude.min(), region[0])
+    npt.assert_allclose(survey.longitude.max(), region[1])
+    npt.assert_allclose(survey.latitude.min(), region[2])
+    npt.assert_allclose(survey.latitude.max(), region[3])
+    assert survey.height.min() <= 0.0
+    assert survey.height.max() >= 2052.2
 
 
-def test_cut_region_airborne_survey():
+def test_subsection_airborne_survey():
     """
     Test if a different cut_region produces a different airborne survey
     """
-    region = (-50e3, 50e3, -50e3, 50e3)
-    cut_region = (-7, -2, 53, 58)
-    survey = airborne_survey(region, cut_region=cut_region)
-    assert survey.easting.size > 5673
-    npt.assert_allclose(survey.easting.min(), region[0])
-    npt.assert_allclose(survey.easting.max(), region[1])
-    npt.assert_allclose(survey.northing.min(), region[2])
-    npt.assert_allclose(survey.northing.max(), region[3])
-    assert survey.elevation.min() <= 359.0
-    assert survey.elevation.max() >= 1255.0
+    subsection = (-7, -2, 53, 58)  # a bigger subsection than the default one
+    region = (-10.1, 9.7, -20.3, -10.5)  # a random region to scale the survey
+    survey = airborne_survey(region=region, subsection=subsection)
+    assert survey.longitude.size > 5673
+    npt.assert_allclose(survey.longitude.min(), region[0])
+    npt.assert_allclose(survey.longitude.max(), region[1])
+    npt.assert_allclose(survey.latitude.min(), region[2])
+    npt.assert_allclose(survey.latitude.max(), region[3])
+    assert survey.height.min() <= 359.0
+    assert survey.height.max() >= 1255.0
