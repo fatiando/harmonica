be52bb6050dc6904c5ca9b0a0cd34109978adaaf
Santiago Soler <santiago.r.soler@gmail.com>
2019-11-08 15:23:18 -0300

    Shrink docstrings to 79 characters
    Also remove unwanted dots at the end of the first line of docstrings.

--- harmonica/synthetic/surveys.py
+++ harmonica/synthetic/surveys.py
@@ -9,35 +9,37 @@ from ..datasets import fetch_britain_magnetic, fetch_south_africa_gravity
 
 def airborne_survey(region=None, data_region=(-5.0, -4.0, 56.0, 56.5)):
     """
-    Create measurement locations for a synthetic airborne survey.
+    Create measurement locations for a synthetic airborne survey
 
-    The observation points are sampled from the Great Britain total-field magnetic
-    anomaly dataset (see :func:`harmonica.datasets.fetch_britain_magnetic`).
-    A portion of the original survey is cut (*data_region*) and the coordinates may be
-    scaled to the given *region*.
+    The observation points are sampled from the Great Britain total-field
+    magnetic anomaly dataset (see
+    :func:`harmonica.datasets.fetch_britain_magnetic`). A portion of the
+    original survey is cut (*data_region*) and the coordinates may be scaled to
+    the given *region*.
 
     Parameters
     ----------
     region : tuple or list (optional)
-        Survey horizontal coordinates will be scaled to span this area.
-        The boundaries must be passed in the following order:
-        (``east``, ``west``, ``south``, ``north``, ...), defined on a geodetic
-        coordinate system and in degrees.
-        Only the 4 horizontal boundaries are used. Subsequent boundaries will be ignored.
-        If ``None``, the survey points won't be scaled. Default ``None``.
+        Survey horizontal coordinates will be scaled to span this area. The
+        boundaries must be passed in the following order: (``east``, ``west``,
+        ``south``, ``north``, ...), defined on a geodetic coordinate system and
+        in degrees. Only the 4 horizontal boundaries are used. Subsequent
+        boundaries will be ignored. If ``None``, the survey points won't be
+        scaled. Default ``None``.
     data_region : tuple or list (optional)
-        Subsection of the original Great Britain magnetic dataset that will be sampled.
-        The boundaries must be passed in the following order:
+        Subsection of the original Great Britain magnetic dataset that will be
+        sampled. The boundaries must be passed in the following order:
         (``east``, ``west``, ``south``, ``north``, ...), defined on a geodetic
-        coordinate system and in degrees. All subsequent boundaries will be ignored.
+        coordinate system and in degrees. All subsequent boundaries will be
+        ignored.
 
     Returns
     -------
     survey : :class:`pandas.DataFrame`
-        Dataframe containing the coordinates of the observation points on a geodetic
-        coordinate system. The :class:`pandas.DataFrame` will have the following
-        columns: ``longitude``, ``latitude``, ``height``. Longitudes and latitudes are
-        in degrees, and heights in meters.
+        Dataframe containing the coordinates of the observation points on
+        a geodetic coordinate system. The :class:`pandas.DataFrame` will have the
+        following columns: ``longitude``, ``latitude``, ``height``. Longitudes and
+        latitudes are in degrees, and heights in meters.
 
     See also
     --------
@@ -65,36 +67,36 @@ def ground_survey(region=None, data_region=(13.60, 20.30, -24.20, -17.5)):
     Create a synthetic ground survey
 
     The observation points are sampled from the South Africa gravity dataset
-    (see :func:`harmonica.datasets.fetch_south_africa_gravity`).
-    Only a portion of the original survey is sampled and its region may be scaled to the
+    (see :func:`harmonica.datasets.fetch_south_africa_gravity`). Only a portion
+    of the original survey is sampled and its region may be scaled to the
     passed ``region``.
 
     Parameters
     ----------
     region : tuple or list (optional)
-        Region at which the survey points coordinates will be scaled.
-        The boundaries must be passed in the following order:
-        (``east``, ``west``, ``south``, ``north``, ...), defined on a geodetic
-        coordinate system and in degrees.
-        All subsequent boundaries will be ignored.
-        If ``None``, the survey points won't be scaled. Default ``None``.
+        Region at which the survey points coordinates will be scaled. The
+        boundaries must be passed in the following order: (``east``, ``west``,
+        ``south``, ``north``, ...), defined on a geodetic coordinate system and
+        in degrees. All subsequent boundaries will be ignored. If ``None``, the
+        survey points won't be scaled. Default ``None``.
     data_region : tuple or list (optional)
-        Region where the original Great Britain magnetic dataset will be sampled.
-        The boundaries must be passed in the following order:
+        Region where the original Great Britain magnetic dataset will be
+        sampled. The boundaries must be passed in the following order:
         (``east``, ``west``, ``south``, ``north``, ...), defined on a geodetic
-        coordinate system and in degrees. All subsequent boundaries will be ignored.
+        coordinate system and in degrees. All subsequent boundaries will be
+        ignored.
 
     Returns
     -------
     survey : :class:`pandas.DataFrame`
-        Dataframe containing the coordinates of the observation points on a geodetic
-        coordinate system. The :class:`pandas.DataFrame` will have the following
-        columns: ``longitude``, ``latitude``, ``height``. Longitudes and latitudes are
-        in degrees, and heights in meters.
+        Dataframe containing the coordinates of the observation points on
+        a geodetic coordinate system. The :class:`pandas.DataFrame` will have the
+        following columns: ``longitude``, ``latitude``, ``height``. Longitudes and
+        latitudes are in degrees, and heights in meters.
 
     See also
     --------
-    datasets.fetch_south_africa_gravity: Fetch gravity station data from South Africa.
+    datasets.fetch_south_africa_gravity
     """
     # Sanity checks for region and data_region
     if region is not None:
@@ -114,32 +116,32 @@ def ground_survey(region=None, data_region=(13.60, 20.30, -24.20, -17.5)):
 
 def _cut_and_scale(survey, region, data_region):
     """
-    Cut a subsection from the original survey and scale it to the given region.
+    Cut a subsection from the original survey and scale it to the given region
 
     Parameters
     ----------
     survey : :class:`pandas.DataFrame`
-        Original survey as a :class:`pandas.DataFrame` containing the following columns:
-        ``longitude``, ``latitude`` and ``height``.
+        Original survey as a :class:`pandas.DataFrame` containing the following
+        columns: ``longitude``, ``latitude`` and ``height``.
     region : tuple or list (optional)
-        Region to which the survey points coordinates will be scaled.
-        The boundaries must be passed in the following order:
-        (``east``, ``west``, ``south``, ``north``, ...), defined on a geodetic
-        coordinate system and in degrees.
-        All subsequent boundaries will be ignored.
-        If ``None``, the survey points won't be scaled.
+        Region to which the survey points coordinates will be scaled. The
+        boundaries must be passed in the following order: (``east``, ``west``,
+        ``south``, ``north``, ...), defined on a geodetic coordinate system and
+        in degrees. All subsequent boundaries will be ignored. If ``None``, the
+        survey points won't be scaled.
     data_region : tuple or list (optional)
-        Region where the original Great Britain magnetic dataset will be sampled.
-        The boundaries must be passed in the following order:
+        Region where the original Great Britain magnetic dataset will be
+        sampled. The boundaries must be passed in the following order:
         (``east``, ``west``, ``south``, ``north``, ...), defined on a geodetic
-        coordinate system and in degrees. All subsequent boundaries will be ignored.
+        coordinate system and in degrees. All subsequent boundaries will be
+        ignored.
 
     Returns
     -------
     survey : :class:`pandas.DataFrame`
-        Dataframe containing the coordinates of the observation points on a geodetic
-        coordinate system. Longitudes and latitudes are in degrees, and heights in
-        meters.
+        Dataframe containing the coordinates of the observation points on
+        a geodetic coordinate system. Longitudes and latitudes are in degrees, and
+        heights in meters.
     """
     # Cut the data into the data_region
     inside_points = inside((survey.longitude, survey.latitude), data_region)
