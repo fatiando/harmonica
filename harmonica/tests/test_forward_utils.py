ff25cbf01dc53c6936f44817500ba83f7942fd42
Santiago Soler <santiago.r.soler@gmail.com>
2020-07-08 19:07:58 -0300

    Add function to compute distance between points in geodetic coordinates

--- harmonica/tests/test_forward_utils.py
+++ harmonica/tests/test_forward_utils.py
@@ -3,6 +3,7 @@ Test utils functions for forward modelling
 """
 import pytest
 import numpy.testing as npt
+import boule as bl
 
 from ..forward.utils import distance, check_coordinate_system
 
@@ -18,6 +19,13 @@ def test_distance():
     point_a = (32.3, 40.1, 1e4)
     point_b = (32.3, 40.1, 1e4 + 100)
     npt.assert_allclose(distance(point_a, point_b, coordinate_system="spherical"), 100)
+    # Geodetic coordinate system
+    point_a = (-71.3, 33.5, 1e4)
+    point_b = (-71.3, 33.5, 1e4 + 100)
+    npt.assert_allclose(
+        distance(point_a, point_b, coordinate_system="geodetic", ellipsoid=bl.WGS84),
+        100,
+    )
 
 
 def test_distance_invalid_coordinate_system():
@@ -32,3 +40,27 @@ def test_check_coordinate_system():
     "Check if invalid coordinate system is passed to _check_coordinate_system"
     with pytest.raises(ValueError):
         check_coordinate_system("this-is-not-a-valid-coordinate-system")
+
+
+def test_geodetic_distance_vs_spherical():
+    """
+    Compare geodetic distance vs spherical distance after conversion
+
+    Test if the closed-form formula for computing the Euclidean distance
+    between two points given in geodetic coordinates is equal to the same
+    distance computed by converting the points to spherical coordinates and
+    using the ``distance_spherical`` function.
+    """
+    # Initialize the WGS84 ellipsoid
+    ellipsoid = bl.WGS84
+    # Define two points in geodetic coordinates
+    point_a = (-69.3, -36.4, 405)
+    point_b = (-71.2, -33.3, 1025)
+    # Compute distance using closed-form formula
+    dist = distance(point_a, point_b, coordinate_system="geodetic", ellipsoid=ellipsoid)
+    # Convert points to spherical coordiantes
+    point_a_sph = ellipsoid.geodetic_to_spherical(point_a)
+    point_b_sph = ellipsoid.geodetic_to_spherical(point_b)
+    # Compute distance using these converted points
+    dist_sph = distance(point_a_sph, point_b_sph, coordinate_system="spherical")
+    npt.assert_allclose(dist, dist_sph)
