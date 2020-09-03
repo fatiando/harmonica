3bec013788492be9c4a22d5d09b940d26e13ac2f
Santiago Soler <santiago.r.soler@gmail.com>
2020-09-03 13:18:59 -0300

    Add prisms_layer and PrismsLayer to doc/api/index.rst

--- harmonica/__init__.py
+++ harmonica/__init__.py
@@ -9,7 +9,7 @@ from .gravity_corrections import bouguer_correction
 from .forward.point_mass import point_mass_gravity
 from .forward.tesseroid import tesseroid_gravity
 from .forward.prism import prism_gravity
-from .forward.prisms_layer import prisms_layer
+from .forward.prisms_layer import prisms_layer, PrismsLayer
 from .equivalent_layer.harmonic import EQLHarmonic, EQLHarmonicSpherical
 
 # Get the version number through versioneer
