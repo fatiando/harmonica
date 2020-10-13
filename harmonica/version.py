f3b00096e3640edc4772c706f06cdf97f050abe8
Santiago Soler <santiago.r.soler@gmail.com>
2020-10-13 16:09:05 -0300

    Explicitly set harmonica name when obtaining the version

--- harmonica/version.py
+++ harmonica/version.py
@@ -6,4 +6,4 @@ from pkg_resources import get_distribution
 
 # Append a "v" before the version returned by setuptools_scm so it can look
 # like: v0.1.0
-full_version = "v" + get_distribution(__name__).version
+full_version = "v" + get_distribution("harmonica").version
