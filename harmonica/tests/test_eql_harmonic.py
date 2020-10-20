a06594072a7ebe62263239c661e8f7722b005884
Santiago Soler <santiago.r.soler@gmail.com>
2020-10-20 11:48:14 -0300

    Improve EQL harmonic classes (#190)
    Make EQLHarmonic and EQLHarmonicSpherical independent subclasses of Verde's
    BaseGridder. Add positional upward argument to grid and and profile
    methods of EQLHarmonic. Raise a NotImplementedError when calling the
    scatter method because it will be deprecated on Verde v2.0.0. Raise
    a NotImplementedError when calling EQLHarmonicSpherical.profile method: the
    profile should be created out of great-circle distances rather than absolute
    differences of longitude and latitude angles. Remove the projection argument
    on EQLHarmonicSpherical.grid method because it's intended to be used only on
    Cartesian gridders. Reduce tests on EQLs with Numba disabled to speed them up.
    Add test functions to catch NotImplementedErrors and to check if projection
    is not a valid argument of EQLHarmonicSpherical.grid method.

--- harmonica/tests/test_eql_harmonic.py
+++ harmonica/tests/test_eql_harmonic.py
@@ -1,6 +1,7 @@
 """
 Test the EQLHarmonic gridder
 """
+import warnings
 import pytest
 import numpy as np
 import numpy.testing as npt
@@ -8,11 +9,30 @@ import verde as vd
 import verde.base as vdb
 
 from .. import EQLHarmonic, EQLHarmonicSpherical, point_mass_gravity
-from ..equivalent_layer.harmonic import (
+from ..equivalent_layer.harmonic import greens_func_cartesian
+from ..equivalent_layer.utils import (
     jacobian_numba,
-    greens_func_cartesian,
+    pop_extra_coords,
 )
 from .utils import require_numba
+
+
+def test_pop_extra_coords():
+    """
+    Test _pop_extra_coords private function
+    """
+    # Check if extra_coords is removed from kwargs
+    kwargs = {"bla": 1, "blabla": 2, "extra_coords": 1400.0}
+    with warnings.catch_warnings(record=True) as warn:
+        pop_extra_coords(kwargs)
+        assert len(warn) == 1
+        assert issubclass(warn[0].category, UserWarning)
+    assert "extra_coords" not in kwargs
+
+    # Check if kwargs is not touched if no extra_coords are present
+    kwargs = {"bla": 1, "blabla": 2}
+    pop_extra_coords(kwargs)
+    assert kwargs == {"bla": 1, "blabla": 2}
 
 
 @require_numba
@@ -37,9 +57,24 @@ def test_eql_harmonic_cartesian():
 
     # Gridding onto a denser grid should be reasonably accurate when compared
     # to synthetic values
-    grid = vd.grid_coordinates(region=region, shape=(60, 60), extra_coords=0)
+    upward = 0
+    shape = (60, 60)
+    grid = vd.grid_coordinates(region=region, shape=shape, extra_coords=upward)
     true = point_mass_gravity(grid, points, masses, field="g_z")
     npt.assert_allclose(true, eql.predict(grid), rtol=1e-3)
+
+    # Test grid method
+    grid = eql.grid(upward, shape=shape, region=region)
+    npt.assert_allclose(true, grid.scalars, rtol=1e-3)
+
+    # Test profile method
+    point1 = (region[0], region[2])
+    point2 = (region[0], region[3])
+    profile = eql.profile(point1, point2, upward, shape[0])
+    true = point_mass_gravity(
+        (profile.easting, profile.northing, profile.upward), points, masses, field="g_z"
+    )
+    npt.assert_allclose(true, profile.scalars, rtol=1e-3)
 
 
 def test_eql_harmonic_small_data_cartesian():
@@ -52,7 +87,7 @@ def test_eql_harmonic_small_data_cartesian():
     points = vd.grid_coordinates(region=region, shape=(6, 6), extra_coords=-1e3)
     masses = vd.datasets.CheckerBoard(amplitude=1e13, region=region).predict(points)
     # Define a set of observation points
-    coordinates = vd.grid_coordinates(region=region, shape=(20, 20), extra_coords=0)
+    coordinates = vd.grid_coordinates(region=region, shape=(8, 8), extra_coords=0)
     # Get synthetic data
     data = point_mass_gravity(coordinates, points, masses, field="g_z")
 
@@ -68,9 +103,24 @@ def test_eql_harmonic_small_data_cartesian():
 
     # Gridding at higher altitude should be reasonably accurate when compared
     # to synthetic values
-    grid = vd.grid_coordinates(region=region, shape=(20, 20), extra_coords=20)
+    upward = 20
+    shape = (8, 8)
+    grid = vd.grid_coordinates(region=region, shape=shape, extra_coords=upward)
     true = point_mass_gravity(grid, points, masses, field="g_z")
-    npt.assert_allclose(true, eql.predict(grid), rtol=0.05)
+    npt.assert_allclose(true, eql.predict(grid), rtol=0.08)
+
+    # Test grid method
+    grid = eql.grid(upward, shape=shape, region=region)
+    npt.assert_allclose(true, grid.scalars, rtol=0.08)
+
+    # Test profile method
+    point1 = (region[0], region[2])
+    point2 = (region[0], region[3])
+    profile = eql.profile(point1, point2, upward, 10)
+    true = point_mass_gravity(
+        (profile.easting, profile.northing, profile.upward), points, masses, field="g_z"
+    )
+    npt.assert_allclose(true, profile.scalars, rtol=0.05)
 
 
 def test_eql_harmonic_custom_points_cartesian():
@@ -83,27 +133,29 @@ def test_eql_harmonic_custom_points_cartesian():
     points = vd.grid_coordinates(region=region, shape=(6, 6), extra_coords=-1e3)
     masses = vd.datasets.CheckerBoard(amplitude=1e13, region=region).predict(points)
     # Define a set of observation points
-    coordinates = vd.grid_coordinates(region=region, shape=(20, 20), extra_coords=0)
+    coordinates = vd.grid_coordinates(region=region, shape=(5, 5), extra_coords=0)
     # Get synthetic data
     data = point_mass_gravity(coordinates, points, masses, field="g_z")
 
-    # The interpolation should be perfect on the data points
-    src_points = tuple(
+    # Pass a custom set of point sources
+    points_custom = tuple(
         i.ravel()
-        for i in vd.grid_coordinates(region=region, shape=(20, 20), extra_coords=-550)
+        for i in vd.grid_coordinates(region=region, shape=(3, 3), extra_coords=-550)
     )
-    eql = EQLHarmonic(points=src_points)
+    eql = EQLHarmonic(points=points_custom)
     eql.fit(coordinates, data)
-    npt.assert_allclose(data, eql.predict(coordinates), rtol=1e-5)
 
     # Check that the proper source locations were set
-    npt.assert_allclose(src_points, eql.points_, rtol=1e-5)
+    npt.assert_allclose(points_custom, eql.points_, rtol=1e-5)
 
-    # Gridding at higher altitude should be reasonably accurate when compared
-    # to synthetic values
-    grid = vd.grid_coordinates(region=region, shape=(20, 20), extra_coords=20)
-    true = point_mass_gravity(grid, points, masses, field="g_z")
-    npt.assert_allclose(true, eql.predict(grid), rtol=0.05)
+
+def test_eql_harmonic_scatter_not_implemented():
+    """
+    Check if scatter method raises a NotImplementedError
+    """
+    eql = EQLHarmonic()
+    with pytest.raises(NotImplementedError):
+        eql.scatter()
 
 
 @pytest.mark.use_numba
@@ -163,11 +215,17 @@ def test_eql_harmonic_spherical():
 
     # Gridding onto a denser grid should be reasonably accurate when compared
     # to synthetic values
-    grid = vd.grid_coordinates(region=region, shape=(60, 60), extra_coords=radius)
+    upward = radius
+    shape = (60, 60)
+    grid = vd.grid_coordinates(region=region, shape=shape, extra_coords=upward)
     true = point_mass_gravity(
         grid, points, masses, field="g_z", coordinate_system="spherical"
     )
     npt.assert_allclose(true, eql.predict(grid), rtol=1e-3)
+
+    # Test grid method
+    grid = eql.grid(upward, shape=shape, region=region)
+    npt.assert_allclose(true, grid.scalars, rtol=1e-3)
 
 
 def test_eql_harmonic_small_data_spherical():
@@ -183,9 +241,7 @@ def test_eql_harmonic_small_data_spherical():
     )
     masses = vd.datasets.CheckerBoard(amplitude=1e13, region=region).predict(points)
     # Define a set of observation points
-    coordinates = vd.grid_coordinates(
-        region=region, shape=(20, 20), extra_coords=radius
-    )
+    coordinates = vd.grid_coordinates(region=region, shape=(8, 8), extra_coords=radius)
     # Get synthetic data
     data = point_mass_gravity(
         coordinates, points, masses, field="g_z", coordinate_system="spherical"
@@ -203,11 +259,17 @@ def test_eql_harmonic_small_data_spherical():
 
     # Gridding at higher altitude should be reasonably accurate when compared
     # to synthetic values
-    grid = vd.grid_coordinates(region=region, shape=(20, 20), extra_coords=radius + 2e3)
+    upward = radius + 2e3
+    shape = (8, 8)
+    grid = vd.grid_coordinates(region=region, shape=shape, extra_coords=upward)
     true = point_mass_gravity(
         grid, points, masses, field="g_z", coordinate_system="spherical"
     )
     npt.assert_allclose(true, eql.predict(grid), rtol=0.05)
+
+    # Test grid method
+    grid = eql.grid(upward, shape=shape, region=region)
+    npt.assert_allclose(true, grid.scalars, rtol=0.05)
 
 
 def test_eql_harmonic_custom_points_spherical():
@@ -223,32 +285,48 @@ def test_eql_harmonic_custom_points_spherical():
     )
     masses = vd.datasets.CheckerBoard(amplitude=1e13, region=region).predict(points)
     # Define a set of observation points
-    coordinates = vd.grid_coordinates(
-        region=region, shape=(20, 20), extra_coords=radius
-    )
+    coordinates = vd.grid_coordinates(region=region, shape=(5, 5), extra_coords=radius)
     # Get synthetic data
     data = point_mass_gravity(
         coordinates, points, masses, field="g_z", coordinate_system="spherical"
     )
 
-    # The interpolation should be perfect on the data points
-    src_points = tuple(
+    # Pass a custom set of point sources
+    points_custom = tuple(
         i.ravel()
         for i in vd.grid_coordinates(
-            region=region, shape=(20, 20), extra_coords=radius - 500e3
+            region=region, shape=(3, 3), extra_coords=radius - 500e3
         )
     )
-    eql = EQLHarmonicSpherical(points=src_points)
+    eql = EQLHarmonicSpherical(points=points_custom)
     eql.fit(coordinates, data)
-    npt.assert_allclose(data, eql.predict(coordinates), rtol=1e-5)
 
     # Check that the proper source locations were set
-    npt.assert_allclose(src_points, eql.points_, rtol=1e-5)
+    npt.assert_allclose(points_custom, eql.points_, rtol=1e-5)
 
-    # Gridding at higher altitude should be reasonably accurate when compared
-    # to synthetic values
-    grid = vd.grid_coordinates(region=region, shape=(20, 20), extra_coords=radius + 2e3)
-    true = point_mass_gravity(
-        grid, points, masses, field="g_z", coordinate_system="spherical"
-    )
-    npt.assert_allclose(true, eql.predict(grid), rtol=0.05)
+
+def test_eql_harmonic_spherical_scatter_not_implemented():
+    """
+    Check if scatter method raises a NotImplementedError
+    """
+    eql = EQLHarmonicSpherical()
+    with pytest.raises(NotImplementedError):
+        eql.scatter()
+
+
+def test_eql_harmonic_spherical_profile_not_implemented():
+    """
+    Check if scatter method raises a NotImplementedError
+    """
+    eql = EQLHarmonicSpherical()
+    with pytest.raises(NotImplementedError):
+        eql.profile(point1=(1, 1), point2=(2, 2), size=3)
+
+
+def test_eql_harmonic_spherical_no_projection():
+    """
+    Check if projection is not a valid argument of grid method
+    """
+    eql = EQLHarmonicSpherical()
+    with pytest.raises(TypeError):
+        eql.grid(upward=10, projection=lambda a, b: (a * 2, b * 2))
