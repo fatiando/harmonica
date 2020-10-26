170f97a10f7c2bcb787b6aca0d5ea91ba40ce8f2
Andrea Balza <andreabalzam@gmail.com>
2020-10-26 11:07:47 -0600

    Included test below with Africa Data Set
    I am using the "scale" xarray variable to store values of continents and oceans but I get an error in line 79.

--- harmonica/isostasy_pratt.py
+++ harmonica/isostasy_pratt.py
@@ -74,22 +74,22 @@ def isostasy_pratt(
     oceans = np.array(topography < 0)
     continent = np.logical_not(oceans)
     scale = np.full(topography.shape, np.nan, dtype="float") #makes empty arrays of scale
-    continent_v = topography*continent
-    oceans_v = topography*oceans
-    cont = density_crust * comp_depth / (continent_v + comp_depth) #not sure about the continent variable
-    oce = (density_crust*comp_depth - density_water*oceans_v)/ (comp_depth-oceans_v)
-    mohod = cont+oce
-    if isinstance(mohod, xr.DataArray):
-        moho.name = "moho_density"
-        moho.attrs["isostasy"] = "Pratt"
-        moho.attrs["density_crust"] = str(density_crust)
-        moho.attrs["comp_depth"] = str(comp_depth)
-        moho.attrs["density_water"] = str(density_water)
-        moho.attrs["reference_depth"] = str(reference_depth)
-    return mohod
+    continent_v = topography*continent #only values where continents are
+    oceans_v = topography*oceans*-1 #positive values 
+    scale[continent_v] = density_crust * comp_depth / (continent_v + comp_depth) 
+    scale[oceans_v] = (density_crust*comp_depth - density_water*oceans_v)/ (comp_depth-oceans_v)
+    rhot = scale
+    if isinstance(rhot, xr.DataArray):
+        rhot.name = "moho_density"
+        rhot.attrs["isostasy"] = "Pratt"
+        rhot.attrs["density_crust"] = str(density_crust)
+        rhot.attrs["comp_depth"] = str(comp_depth)
+        rhot.attrs["density_water"] = str(density_water)
+        rhot.attrs["reference_depth"] = str(reference_depth)
+    return rhot
 
-##TESTING 
 
+"TESTING WITHOUT NAMING FUNCTION TO SEE IF ALL PARTS WORK
 
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
@@ -100,76 +100,42 @@ import harmonica as hm
 data = hm.datasets.fetch_topography_earth()
 region = (-20, 60, -40, 45)
 data_africa = data.sel(latitude=slice(*region[2:]), longitude=slice(*region[:2]))
-print("Topography/bathymetry grid:")
-print(data_africa)
 
-# Calculate the isostatic Moho depth using the default values for densities and
-# reference Moho
-moho = isostasy_pratt(data_africa.topography)
-print("\nMoho depth grid:")
-print(moho)
+topography=data_africa.topography
+comp_depth=100e3
+density_crust=2.67e3
+density_water=1e3
 
-# Draw the maps
+   # Need to cast to array to make sure numpy indexing works as expected for
+    # 1D DataArray topography
+    oceans = np.array(topography < 0)
+    continent = np.logical_not(oceans)
+    scale = np.full(topography.shape, np.nan, dtype="float") #makes empty arrays of scale
+    continents_v = topography*continent #only values where continents are
+    oceans_v = topography*oceans*-1 #positive values where ocean values are
+    scale[continent_v] = density_crust * comp_depth / (continent_v + comp_depth)
+    scale[oceans_v] = (density_crust*comp_depth - density_water*oceans_v)/ (comp_depth-oceans_v)
+    rhot = cont+oce
+    if isinstance(rhot, xr.DataArray):
+        rhot.name = "moho_density"
+        rhot.attrs["isostasy"] = "Pratt"
+        rhot.attrs["density_crust"] = str(density_crust)
+        rhot.attrs["comp_depth"] = str(comp_depth)
+        rhot.attrs["density_water"] = str(density_water)
+
+
 plt.figure(figsize=(8, 9.5))
-ax = plt.axes(projection=ccrs.LambertCylindrical(central_longitude=20))
-pc = moho.plot.pcolormesh(
+ax = scale.axes(projection=ccrs.LambertCylindrical(central_longitude=20))
+pc = continent_v.plot.pcolormesh(
     ax=ax, cmap="viridis_r", add_colorbar=True, transform=ccrs.PlateCarree()
 )
-plt.colorbar(pc, ax=ax, orientation="horizontal", pad=0.01, aspect=50, label="meters")
+plt.colorbar(pc, ax=ax, orientation="horizontal", pad=0.01, aspect=50, label="rho kg/m3")
 ax.coastlines()
-ax.set_title("Airy isostatic Moho depth of Africa")
+ax.set_title("pratt isostatic density of Africa")
 ax.set_extent(region, crs=ccrs.PlateCarree())
 plt.show()
-plt.savefig('airy.jpg')
+plt.savefig('pratt.jpg')
 
 
-"""
-Airy Isostasy
-=============
-
-According to the Airy hypothesis of isostasy, topography above sea level is
-supported by a thickening of the crust (a root) while oceanic basins are
-supported by a thinning of the crust (an anti-root). Function
-:func:`harmonica.isostasy_airy` computes the depth to crust-mantle interface
-(the Moho) according to Airy isostasy. One must assume a value for the
-reference thickness of the continental crust in order to convert the
-root/anti-root thickness into Moho depth. The function contains common default
-values for the reference thickness and crust, mantle, and water densities
-[TurcotteSchubert2014]_.
-
-We'll use our sample topography data
-(:func:`harmonica.datasets.fetch_topography_earth`) to calculate the Airy
-isostatic Moho depth of Africa.
-"""
-import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
-import harmonica as hm
-
-# Load the elevation model and cut out the portion of the data corresponding to
-# Africa
-data = hm.datasets.fetch_topography_earth()
-region = (-20, 60, -40, 45)
-data_africa = data.sel(latitude=slice(*region[2:]), longitude=slice(*region[:2]))
-print("Topography/bathymetry grid:")
-print(data_africa)
-
-# Calculate the isostatic Moho depth using the default values for densities and
-# reference Moho
-moho = hm.isostasy_airy(data_africa.topography)
-print("\nMoho depth grid:")
-print(moho)
-
-# Draw the maps
-plt.figure(figsize=(8, 9.5))
-ax = plt.axes(projection=ccrs.LambertCylindrical(central_longitude=20))
-pc = moho.plot.pcolormesh(
-    ax=ax, cmap="viridis_r", add_colorbar=True, transform=ccrs.PlateCarree()
-)
-plt.colorbar(pc, ax=ax, orientation="horizontal", pad=0.01, aspect=50, label="meters")
-ax.coastlines()
-ax.set_title("Airy isostatic Moho depth of Africa")
-ax.set_extent(region, crs=ccrs.PlateCarree())
-plt.show()
-plt.savefig('airy.jpg')
 
 
