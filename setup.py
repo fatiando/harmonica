ae14216d6f97811a23880c7c1ff896d94b410073
Santiago Soler <santiago.r.soler@gmail.com>
2020-10-29 12:00:28 -0300

    Add license and copyright notice to every .py file
    Now the script was ran only once.

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2018 The Harmonica Developers.
+# Distributed under the terms of the BSD 3-Clause License.
+# SPDX-License-Identifier: BSD-3-Clause
 """
 Build and install the project.
 
