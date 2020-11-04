36c008365aef6242f76e33b1ec697040e03b005c
Santiago Soler <santiago.r.soler@gmail.com>
2020-11-04 09:09:26 -0300

    Revert "Add license and copyright notice to every .py file"
    This reverts commit 92522ea214b0506a4ce8a19e1ebf49834a4e7965.

--- harmonica/forward/__init__.py
+++ harmonica/forward/__init__.py
@@ -1,8 +1,3 @@
-# Copyright (c) 2018 The Harmonica Developers.
-# Distributed under the terms of the BSD 3-Clause License.
-# SPDX-License-Identifier: BSD-3-Clause
-#
-# This code is part of the Fatiando a Terra project (https://www.fatiando.org)
 """
 Forward modeling functions for gravity and magnetic fields of geometric shapes
 """
