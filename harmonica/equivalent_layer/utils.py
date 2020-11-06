189535f655bcb35e91b8ddf343cd75fa8733be99
Santiago Soler <santiago.r.soler@gmail.com>
2020-11-06 11:08:51 -0300

    Parallelize EQLs predictions and Jacobian build (#203)
    Parallelize the build of the Jacobian matrix and the prediction step on
    equivalent layer gridders through numba.prange iterators. This enables the
    fit and predict methods to use all the available CPUs, greatly reducing the
    computational time. The prange is only applied to the outer loops for each
    task, i.e. distributing computation points between CPUs, but leaving the
    iteration through the sources on single ones.

--- harmonica/equivalent_layer/utils.py
+++ harmonica/equivalent_layer/utils.py
@@ -2,21 +2,21 @@
 Utility functions for equivalent layer gridders
 """
 from warnings import warn
-from numba import jit
+from numba import jit, prange
 
 
-@jit(nopython=True)
+@jit(nopython=True, parallel=True)
 def jacobian_numba(coordinates, points, jac, greens_function):
     """
     Calculate the Jacobian matrix using numba to speed things up.
 
-    It works both for Cartesian and spherical coordiantes.
-    We need to pass the corresponding Green's function through the
-    ``greens_function`` argument.
+    It works both for Cartesian and spherical coordiantes. We need to pass the
+    corresponding Green's function through the ``greens_function`` argument.
+    The Jacobian is built in parallel in order to reduce the computation time.
     """
     east, north, upward = coordinates[:]
     point_east, point_north, point_upward = points[:]
-    for i in range(east.size):
+    for i in prange(east.size):
         for j in range(point_east.size):
             jac[i, j] = greens_function(
                 east[i],
@@ -28,18 +28,20 @@ def jacobian_numba(coordinates, points, jac, greens_function):
             )
 
 
-@jit(nopython=True)
-def predict_numba(coordinates, points, coeffs, result, greens_function):
+@jit(nopython=True, parallel=True)
+def predict_numba(
+    coordinates, points, coeffs, result, greens_function
+):  # pylint: disable=not-an-iterable
     """
     Calculate the predicted data using numba for speeding things up.
 
-    It works both for Cartesian and spherical coordiantes.
-    We need to pass the corresponding Green's function through the
-    ``greens_function`` argument.
+    It works both for Cartesian and spherical coordiantes. We need to pass the
+    corresponding Green's function through the ``greens_function`` argument.
+    The prediction is run in parallel in order to reduce the computation time.
     """
     east, north, upward = coordinates[:]
     point_east, point_north, point_upward = points[:]
-    for i in range(east.size):
+    for i in prange(east.size):
         for j in range(point_east.size):
             result[i] += coeffs[j] * greens_function(
                 east[i],
