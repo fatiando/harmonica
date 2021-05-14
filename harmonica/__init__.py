6253982b1255293e8ee5ef1d5bab297c617e03d6
Santiago Soler <santiago.r.soler@gmail.com>
2021-05-14 14:01:48 -0300

    Ignore pylint import-self on harmonica/__init__.py

--- harmonica/__init__.py
+++ harmonica/__init__.py
@@ -4,7 +4,8 @@
 #
 # This code is part of the Fatiando a Terra project (https://www.fatiando.org)
 #
-# pylint: disable=missing-docstring,import-outside-toplevel
+# pylint: disable=missing-docstring,import-outside-toplevel,import-self
+#
 # Import functions/classes to make the public API
 from . import datasets
 from . import synthetic
