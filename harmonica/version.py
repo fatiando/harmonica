e4682872b122925b7a9f0952fe5f12c0da157f54
Santiago Soler <santiago.r.soler@gmail.com>
2021-10-21 10:21:21 -0300

    Fix import of Harmonica version on sample datasets (#264)
    Create a new version.py file tha defines the __version__ variable as the
    version number with a leading v. The new __version__ variable is imported
    by __init__.py and by sample_data.py without generating any circular
    import. Solves the issue of docs not being built on new releases.

---
+++ harmonica/version.py
@@ -0,0 +1,8 @@
+"""
+Define the __version__ variable as the version number with leading "v"
+"""
+# This file is generated automatically by setuptools_scm
+from ._version import version
+
+# Add a "v" to the version number
+__version__ = f"v{version}"
