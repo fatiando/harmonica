0415eb1e65fad4a5d88d1762d4b2614009748bbd
Santiago Soler <santiago.r.soler@gmail.com>
2021-12-01 13:32:26 -0300

    Simplify the generation of cells through the dictionary interface

--- harmonica/visualization/prism.py
+++ harmonica/visualization/prism.py
@@ -67,69 +67,22 @@ def prisms_to_pyvista(prisms, properties=None):
         raise ValueError(msg.format("vtk"))
     if pyvista is None:
         raise ValueError(msg.format("pyvista"))
+
     # Get prisms and number of prisms
     prisms = np.atleast_2d(prisms)
     n_prisms = prisms.shape[0]
     # Get the vertices of the prisms
     vertices = np.vstack([_get_prism_vertices(prism) for prism in prisms])
-    # Get the cells for these prisms
-    cells = _build_cells(n_prisms)
-    # Define an array with the types of the cells
-    cell_type = np.full(n_prisms, vtk.VTK_HEXAHEDRON)
+    # Generate the cells for the hexahedrons
+    cells = np.arange(n_prisms * 8).reshape([n_prisms, 8])
     # Build the UnstructuredGrid
-    pv_grid = pyvista.UnstructuredGrid(cells, cell_type, vertices)
+    pv_grid = pyvista.UnstructuredGrid({vtk.VTK_HEXAHEDRON: cells}, vertices)
     # Add properties to the grid
     if properties is not None:
         for name, prop in properties.items():
-            # Convert the property to an array and ravel it
-            prop = np.atleast_1d(prop).ravel()
             # Assign the property to the cell_data
-            pv_grid.cell_data[name] = prop
+            pv_grid.cell_data[name] = np.atleast_1d(prop).ravel()
     return pv_grid
-
-
-def _build_cells(n_prisms):
-    """
-    Build the VTK cells for a given number of prisms
-
-    We will represent each prism a single cell of ``VTK_HEXAHEDRON`` type.
-    Each cell is an array with 9 elements:
-      - the first one indicates the number of vertices that the cell will have
-        (8 for prisms), and
-      - the following 8 elements are the indices of the vertices that form the
-        prism.
-
-    The cells is a concatenation of every 9 elements cell array, resulting in
-    a 1d-array of 9 times ``n_prisms`` elements.
-
-    Parameters
-    ----------
-    n_prisms : int
-        Number of prisms in the set
-
-    Returns
-    -------
-    cells : 1d-array
-        Array representing the cells for each one of the prisms.
-
-    Examples
-    --------
-
-    >>> _build_cells(4)
-    array([ 8,  0,  1,  2,  3,  4,  5,  6,  7,  8,  8,  9, 10, 11, 12, 13, 14,
-           15,  8, 16, 17, 18, 19, 20, 21, 22, 23,  8, 24, 25, 26, 27, 28, 29,
-           30, 31])
-    """
-    # Get total number of vertices
-    n_vertices = n_prisms * 8
-    # Build the indices array as a 2D array: each row contains the indices for
-    # each prism
-    indices = np.arange(n_vertices).reshape(n_prisms, 8)
-    # Create a vertical array full of 8
-    n_vertices_per_cell = np.full(fill_value=8, shape=(n_prisms, 1))
-    # Stack the indices and n_vertices_per_cell arrays
-    cells = np.hstack((n_vertices_per_cell, indices)).ravel()
-    return cells
 
 
 def _get_prism_vertices(prism):
