b2f6a7700de0421c11b66a85da71d89724f67c8c
Santiago Soler <santiago.r.soler@gmail.com>
2022-05-03 14:23:52 -0300

    Add test for 2nd order derivatives
    Add tensor fields to the sample-gravity-grid.nc. Save every field in
    mGal and Eotvos (SI conversion is done in test functions).
    Add new test function for derivative_upward using order=2: it compares
    the second derivative of the potential against the g_zz field.

--- harmonica/tests/test_derivatives.py
+++ harmonica/tests/test_derivatives.py
@@ -38,14 +38,38 @@ def test_derivative_upward(sample_gravity_grid):
         "northing": potential.northing.size // 3,
     }
     # need to drop upward coordinate (bug in xrft)
-    potential_padded = xrft.pad(potential.drop("upward"), pad_width=pad_width)
+    potential_padded = xrft.pad(potential.drop_vars("upward"), pad_width=pad_width)
     # Calculate upward derivative and unpad it
     derivative = derivative_upward(potential_padded)
     derivative = xrft.unpad(derivative, pad_width)
     # Compare against g_z (trim the borders to ignore boundary effects)
     trim = 5
-    rms = root_mean_square_error(
-        derivative[trim:-trim, trim:-trim],
-        sample_gravity_grid.g_z[trim:-trim, trim:-trim],
-    )
-    assert rms / np.abs(sample_gravity_grid.g_z).max() < 0.015
+    derivative = derivative[trim:-trim, trim:-trim]
+    g_z = sample_gravity_grid.g_z[trim:-trim, trim:-trim] * 1e-5  # convert to SI units
+    rms = root_mean_square_error(derivative, g_z)
+    assert rms / np.abs(g_z).max() < 0.015
+
+
+def test_derivative_upward_order_2(sample_gravity_grid):
+    """
+    Test higher order of derivative_upward function against the sample grid
+    """
+    potential = sample_gravity_grid.potential
+    # Pad the potential field grid to improve accuracy
+    pad_width = {
+        "easting": potential.easting.size // 3,
+        "northing": potential.northing.size // 3,
+    }
+    # need to drop upward coordinate (bug in xrft)
+    potential_padded = xrft.pad(potential.drop_vars("upward"), pad_width=pad_width)
+    # Calculate second upward derivative and unpad it
+    second_deriv = derivative_upward(potential_padded, order=2)
+    second_deriv = xrft.unpad(second_deriv, pad_width)
+    # Compare against g_zz (trim the borders to ignore boundary effects)
+    trim = 10
+    second_deriv = second_deriv[trim:-trim, trim:-trim]
+    g_zz = (
+        sample_gravity_grid.g_zz[trim:-trim, trim:-trim] * 1e-9
+    )  # convert to SI units
+    rms = root_mean_square_error(second_deriv, g_zz)
+    assert rms / np.abs(g_zz).max() < 0.015
