1d92cbb70f4545c91ecd21e20dd36339cdc674d4
LL-Geo <54405391+LL-Geo@users.noreply.github.com>
2022-06-16 23:09:25 +0800

    Revert "Rename function to isostatic_moho_airy"
    This reverts commit 3e4d14104c7db3c32fb4a3a95a39e935bf1f9d3a.

--- harmonica/isostasy.py
+++ harmonica/isostasy.py
@@ -8,52 +8,50 @@
 Function to calculate the thickness of the roots and antiroots assuming the
 Airy isostatic hypothesis.
 """
+import numpy as np
 import xarray as xr
 
 
 def isostasy_airy(
-    basement_elevation,
-    layer_thickness=(),
-    layer_density=(),
+    topography,
     density_crust=2.8e3,
     density_mantle=3.3e3,
+    density_water=1e3,
     reference_depth=30e3,
 ):
     r"""
-    Calculate the isostatic Moho depth from rock equvalent topography using
-    Airy's hypothesis.
+    Calculate the isostatic Moho depth from topography using Airy's hypothesis.
 
-    According to the Airy hypothesis of isostasy, rock equvalent topography
-    above sea level is supported by a thickening of the crust (a root) while
-    rock equvalent topography below sea level is supported by a thinning of
-    the crust (an anti-root). This assumption is usually
+    According to the Airy hypothesis of isostasy, topography above sea level is
+    supported by a thickening of the crust (a root) while oceanic basins are
+    supported by a thinning of the crust (an anti-root). This assumption is
+    usually
 
-    .. figure:: ../../_static/figures/airy-isostasy.png
+    .. figure:: ../../_static/figures/airy-isostasy.svg
         :align: center
         :width: 400px
 
         *Schematic of isostatic compensation following the Airy hypothesis.*
 
-    The relationship between the rock equvalent topography (:math:`ret`)
+    The relationship between the topographic/bathymetric heights (:math:`h`)
     and the root thickness (:math:`r`) is governed by mass balance relations
     and can be found in classic textbooks like [TurcotteSchubert2014]_ and
     [Hofmann-WellenhofMoritz2006]_.
 
-    Compress all layers's mass above basement (:math:`h`) into rock equvalent
-    topography [Balmino_etal1973]_ :
+    On the continents (positive topographic heights):
 
     .. math ::
 
-        ret = h + \frac{\rho_{i}}{\rho_{c}}th_{i} + ..
+        r = \frac{\rho_{c}}{\rho_m - \rho_{c}} h
 
-    Based on rock equvalent topography, the root is calculated as:
+    while on the oceans (negative topographic heights):
 
     .. math ::
-        r = \frac{\rho_{c}}{\rho_m - \rho_{c}} ret
+        r = \frac{\rho_{c} - \rho_w}{\rho_m - \rho_{c}} h
 
-    in which :math:`ret` is the rock equvalent topography , :math:`\rho_m` is
-    the density of the mantle, and :math:`\rho_{c}` is the density of the
-    crust.
+    in which :math:`h` is the topography/bathymetry, :math:`\rho_m` is the
+    density of the mantle, :math:`\rho_w` is the density of the water, and
+    :math:`\rho_{c}` is the density of the crust.
 
     The computed root thicknesses will be added to the given reference Moho
     depth (:math:`H`) to arrive at the isostatic Moho depth. Use
@@ -62,18 +60,10 @@ def isostasy_airy(
 
     Parameters
     ----------
-    basement_elevation : array or :class:`xarray.DataArray`
-        Basement elevation in meters. It usually refer to topography height
-        and bathymetry depth without sediment cover. When considering
-        sedimentary layer, it refer to crystalline basement. It is usually
-        prudent to use floating point values instead of integers to avoid
-        integer division errors.
-    layer_thickness : tuple contains `xarray.DataArray`
-        Layer thickness in meters. It refer to all layers above
-        topography/basement, including ice, water, and sediment.
-    layer_density : tuple contains `xarray.DataArray` or float
-        Layer density in :math:`kg/m^3`. It refer to all layers above
-        topography/basement, including ice, water, and sediment.
+    topography : array or :class:`xarray.DataArray`
+        Topography height and bathymetry depth in meters. It is usually prudent
+        to use floating point values instead of integers to avoid integer
+        division errors.
     density_crust : float
         Density of the crust in :math:`kg/m^3`.
     density_mantle : float
@@ -87,30 +77,21 @@ def isostasy_airy(
     -------
     moho_depth : array or :class:`xarray.DataArray`
          The isostatic Moho depth in meters.
+
     """
-
-    # Define scale factor to calculate Airy root
-    scale = density_crust / (density_mantle - density_crust)
-
-    # For the case: multi-layers above topography/basement
-    if type(layer_thickness) is tuple:
-        # Define total mass of layers abrove topography/basement
-        layer_mass = tuple(
-            ele1 * ele2 for ele1, ele2 in zip(layer_thickness, layer_density)
-        )
-        # Calculate equvalent topography
-        rock_equavalent_topography = (
-            basement_elevation + sum(list(layer_mass)) / density_crust
-        )
-    else:
-        # For the case: a single layer or no layer above topography/basement
-        layer_mass = layer_thickness * layer_density
-        rock_equavalent_topography = basement_elevation + layer_mass / density_crust
-    # Calculate Moho depth
-    moho = rock_equavalent_topography * scale + reference_depth
+    # Need to cast to array to make sure numpy indexing works as expected for
+    # 1D DataArray topography
+    oceans = np.array(topography < 0)
+    continent = np.logical_not(oceans)
+    scale = np.full(topography.shape, np.nan, dtype="float")
+    scale[continent] = density_crust / (density_mantle - density_crust)
+    scale[oceans] = (density_crust - density_water) / (density_mantle - density_crust)
+    moho = topography * scale + reference_depth
     if isinstance(moho, xr.DataArray):
         moho.name = "moho_depth"
         moho.attrs["isostasy"] = "Airy"
         moho.attrs["density_crust"] = str(density_crust)
         moho.attrs["density_mantle"] = str(density_mantle)
+        moho.attrs["density_water"] = str(density_water)
+        moho.attrs["reference_depth"] = str(reference_depth)
     return moho
