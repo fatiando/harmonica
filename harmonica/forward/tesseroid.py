0bcad58698d1e7cd8f562cb61b1274ddaa14ec5d
Agustina Pesce <pesce.agustina@gmail.com>
2022-08-16 12:16:44 -0700

    Add the function to check the tesseroid with zero density or volume

--- harmonica/forward/tesseroid.py
+++ harmonica/forward/tesseroid.py
@@ -17,6 +17,7 @@ from ._tesseroid_utils import (
     _check_tesseroids,
     gauss_legendre_quadrature,
     glq_nodes_weights,
+    _discard_null_tesseroids,
 )
 from ._tesseroid_variable_density import (
     density_based_discretization,
@@ -167,6 +168,10 @@ def tesseroid_gravity(
                 "Number of elements in density ({}) ".format(density.size)
                 + "mismatch the number of tesseroids ({})".format(tesseroids.shape[0])
             )
+        # Discard null tesseroids (zero density or zero volume)
+        tesseroids, density = _discard_null_tesseroids(tesseroids, density)
+        print("Tesseroids", tesseroids.shape)
+        print("density", density.shape)
     # Get GLQ unscaled nodes, weights and number of nodes for each small
     # tesseroid
     glq_nodes, glq_weights = glq_nodes_weights(GLQ_DEGREES)
