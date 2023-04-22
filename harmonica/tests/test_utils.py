fa33358fbc143bbdc97a3da7dd3d1dc52fcd2c24
Agustina Pesce <pesce.agustina@gmail.com>
2023-04-21 18:55:50 -0700

    Run format

--- harmonica/tests/test_utils.py
+++ harmonica/tests/test_utils.py
@@ -1,26 +1,26 @@
-import numpy.testing as npt
+# Copyright (c) 2018 The Harmonica Developers.
+# Distributed under the terms of the BSD 3-Clause License.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# This code is part of the Fatiando a Terra project (https://www.fatiando.org)
+#
 import numpy as np
+import numpy.testing as npt
 import pytest
-from .. import magnetic_ang_to_vec, magnetic_vec_to_ang
 
+from .. import magnetic_ang_to_vec, magnetic_vec_to_ang
 
 VECTORS = [
     [0.5, 0.5, -0.70710678],
     [0.5, 0.5, 0.70710678],
     [-0.5, 0.5, -0.70710678],
-    [0, 0, -1],   # Over -z axis
-    [1, 0, 0],    # Over east (y) axis
-    [0, 1, 0],    # Over north (x) axis
+    [0, 0, -1],  # Over -z axis
+    [1, 0, 0],  # Over east (y) axis
+    [0, 1, 0],  # Over north (x) axis
 ]
 
-ANGLES = [
-    [1, 45, 45],
-    [1, -45, 45.],
-    [1, 45, -45],
-    [1, 90, 0],
-    [1, 0, 90],
-    [1, 0, 0]
-]
+ANGLES = [[1, 45, 45], [1, -45, 45.0], [1, 45, -45], [1, 90, 0], [1, 0, 90], [1, 0, 0]]
+
 
 @pytest.fixture(name="data")
 def create_angles_vectors():
@@ -30,6 +30,7 @@ def create_angles_vectors():
     intensity, inclination, declination = np.vstack(ANGLES).T
     magnetic_e, magnetic_n, magnetic_u = np.vstack(VECTORS).T
     return intensity, inclination, declination, magnetic_e, magnetic_n, magnetic_u
+
 
 def test_magnetic_ang_to_vec_float():
     """
@@ -41,8 +42,9 @@ def test_magnetic_ang_to_vec_float():
         magnetic_e, magnetic_n, magnetic_u = VECTORS[i]
         npt.assert_almost_equal(
             magnetic_ang_to_vec(intensity, inclination, declination),
-            (magnetic_e, magnetic_n, magnetic_u)
+            (magnetic_e, magnetic_n, magnetic_u),
         )
+
 
 def test_magnetic_vec_to_ang_float():
     """
@@ -54,8 +56,9 @@ def test_magnetic_vec_to_ang_float():
         magnetic_e, magnetic_n, magnetic_u = VECTORS[i]
         npt.assert_allclose(
             magnetic_vec_to_ang(magnetic_e, magnetic_n, magnetic_u),
-            (intensity, inclination, declination)
+            (intensity, inclination, declination),
         )
+
 
 def test_magnetic_ang_to_vec_array(data):
     """
@@ -66,8 +69,9 @@ def test_magnetic_ang_to_vec_array(data):
     magnetic_e, magnetic_n, magnetic_u = data[3:]
     npt.assert_almost_equal(
         magnetic_ang_to_vec(intensity, inclination, declination),
-        (magnetic_e, magnetic_n, magnetic_u)
+        (magnetic_e, magnetic_n, magnetic_u),
     )
+
 
 def test_magnetic_vec_to_ang_array(data):
     """
@@ -78,8 +82,9 @@ def test_magnetic_vec_to_ang_array(data):
     magnetic_e, magnetic_n, magnetic_u = data[3:]
     npt.assert_allclose(
         magnetic_vec_to_ang(magnetic_e, magnetic_n, magnetic_u),
-        (intensity, inclination, declination)
+        (intensity, inclination, declination),
     )
+
 
 def test_unity(data):
     magnetic_e, magnetic_n, magnetic_u = data[3:]
