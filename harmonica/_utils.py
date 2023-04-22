ebfddaa78f930fc21489c5bd625feff7a93f395d
Agustina Pesce <pesce.agustina@gmail.com>
2023-04-21 18:56:25 -0700

    Improve docstring and comments

--- harmonica/_utils.py
+++ harmonica/_utils.py
@@ -1,14 +1,21 @@
+# Copyright (c) 2018 The Harmonica Developers.
+# Distributed under the terms of the BSD 3-Clause License.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# This code is part of the Fatiando a Terra project (https://www.fatiando.org)
+#
 import numpy as np
 
 
 def magnetic_ang_to_vec(intensity, inclination, declination):
     """
-    Convert intensity, inclination and declination angles to a 3-component magnetic
-    vector.
+    Convert intensity, inclination and declination angles of the magnetic field to a
+    3-component magnetic vector.
 
     .. note::
-        Inclination is measured positive downward from the horizontal plane and
-        declination is measured with respect to North and it is positive east.
+        Inclination is measured positive downward from the horizontal plane, and
+        declination is measured with respect to north, where positive angles indicate
+        east.
 
     Parameters
     ----------
@@ -17,15 +24,18 @@ def magnetic_ang_to_vec(intensity, inclination, declination):
     inclination : float or array
         Inclination angle of the magnetic vector in degree.
         It mast be in ``degrees``.
-        If ``degrees`` is False, then it's returned in radians.
     declination : float or array
         Declination angle of the magnetic vector.
         It mast be in ``degrees``.
 
     Returns
     -------
-    magnetic_vector : array = [magnetic_e, magnetic_n, magnetic_u]
-        Magnetic vector.
+     magnetic_e : float or array
+        Easting component of the magnetic vector.
+    magnetic_n : float or array
+        Northing component of the magnetic vector.
+    magnetic_u : float or array
+        Upward component of the magnetic vector.
 
     Examples
     --------
@@ -33,12 +43,15 @@ def magnetic_ang_to_vec(intensity, inclination, declination):
     >>> print(magnetic_e, magnetic_n, magnetic_u)
     1.5, 1.5000000000000002, -2.1213203435596424
     """
+    # Transform to radians
     inc_rad = np.radians(inclination)
     dec_rad = np.radians(declination)
+    # Calculate the 3 components
     magnetic_e = intensity * np.cos(inc_rad) * np.sin(dec_rad)
     magnetic_n = intensity * np.cos(inc_rad) * np.cos(dec_rad)
-    magnetic_u = - intensity *  np.sin(inc_rad)
+    magnetic_u = -intensity * np.sin(inc_rad)
     return magnetic_e, magnetic_n, magnetic_u
+
 
 def magnetic_vec_to_ang(magnetic_e, magnetic_n, magnetic_u, degrees=True):
     r"""
@@ -106,18 +119,18 @@ def magnetic_vec_to_ang(magnetic_e, magnetic_n, magnetic_u, degrees=True):
     >>> print(intensity, inc, dec)
     2.999999757066657, 44.99999536031435, 45.00000000000001
     """
-    # Compute the intensity
+    # Compute the intensity as a norm
     vectors = np.vstack((magnetic_e, magnetic_n, magnetic_u)).T
     intensity = np.linalg.norm(vectors, axis=1)
     # Compute the horizontal component of the magnetic vector
     horizontal_component = np.array([np.sqrt(magnetic_e**2 + magnetic_n**2)])
-    # Mask the values equal to zero
-    horizontal_component = np.ma.masked_values(horizontal_component, 0.)
+    # Mask the values equal to zero in the horizontal component
+    horizontal_component = np.ma.masked_values(horizontal_component, 0.0)
     # Calculate the inclination and declination using the mask
-    inclination = np.arctan(- magnetic_u / horizontal_component)
+    inclination = np.arctan(-magnetic_u / horizontal_component)
     declination = np.arcsin(magnetic_e / horizontal_component)
     # Fill the masked values
-    inclination = inclination.filled(- np.sign(magnetic_u) * np.pi / 2)
+    inclination = inclination.filled(-np.sign(magnetic_u) * np.pi / 2)
     declination = declination.filled(0)
     # Convert to degree if needed
     if degrees:
