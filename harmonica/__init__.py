19356fad0394087ba430af94e8fad8607dd2dbf0
Santiago Soler <santisoler@fastmail.com>
2023-07-19 15:56:40 -0700

    Fix wrong import in __init__.py

--- harmonica/__init__.py
+++ harmonica/__init__.py
@@ -28,7 +28,7 @@ from ._transformations import (
     reduction_to_pole,
     upward_continuation,
 )
-from ._utils import magnetic_ang_to_vec, magnetic_vec_to_ang
+from ._utils import magnetic_ang_to_vec, magnetic_vec_to_angles
 from ._version import __version__
 
 
