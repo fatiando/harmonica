Bug fix to calculation of internal magnetic field

harmonica/_forward/ellipsoid_gravity.py
@@ -6,7 +6,7 @@ import numpy as np
 from scipy.constants import gravitational_constant as g
 from scipy.special import ellipeinc, ellipkinc
 
-from .utils_ellipsoids import _calculate_lambda, _get_v_as_Euler
+from .utils_ellipsoids import _calculate_lambda, _get_v_as_euler
 
 
 def ellipsoid_gravity(coordinates, ellipsoids, density, field="g"):
@@ -124,10 +124,11 @@ def ellipsoid_gravity(coordinates, ellipsoids, density, field="g"):
 
         # preserve ellipsoid shape, translate origin of ellipsoid
         cast = np.broadcast(e, n, u)
-        obs_points = np.vstack(((e - ox).ravel(), (n - oy).ravel(), (u - oz).ravel()))
+        obs_points = np.vstack(((e - ox).ravel(), (n - oy).ravel(),
+                                (u - oz).ravel()))
 
         # create rotation matrix
-        r = _get_v_as_Euler(yaw, pitch, roll)
+        r = _get_v_as_euler(yaw, pitch, roll)
 
         # rotate observation points
         rotated_points = r.T @ obs_points
@@ -137,7 +138,8 @@ def ellipsoid_gravity(coordinates, ellipsoids, density, field="g"):
         internal_mask = (x**2) / (a**2) + (y**2) / (b**2) + (z**2) / (c**2) < 1
 
         # calculate gravity component for the rotated points
-        gx, gy, gz = _get_gravity_array(internal_mask, a, b, c, x, y, z, density[index])
+        gx, gy, gz = _get_gravity_array(internal_mask, a, b, c, x, y, z,
+                                        density[index])
         gravity = np.vstack((gx.ravel(), gy.ravel(), gz.ravel()))
 
         # project onto upward unit vector, axis U
@@ -400,14 +402,16 @@ def _get_gravity_prolate(x, y, z, a, b, c, density, lmbda=None):
 
     # compute repeated log_e term
     log_term = np.log(
-        ((a**2 - b**2) ** 0.5 + (a**2 + lmbda) ** 0.5) / ((b**2 + lmbda) ** 0.5)
+        ((a**2 - b**2) ** 0.5 + (a**2 + lmbda) ** 0.5) /
+        ((b**2 + lmbda) ** 0.5)
     )
 
     # compute repeated f_2 second term
     f_2_term_2 = (((a**2 - b**2) * (a**2 + lmbda)) ** 0.5) / (b**2 + lmbda)
 
     # compile terms
-    dg1 = 4 * co_eff1 * x * (((a**2 - b**2) / (a**2 + lmbda)) ** 0.5 - log_term)
+    dg1 = 4 * co_eff1 * x * (((a**2 - b**2) / (a**2 + lmbda))
+                             ** 0.5 - log_term)
     dg2 = 2 * co_eff1 * y * (log_term - f_2_term_2)
     dg3 = 2 * co_eff1 * z * (log_term - f_2_term_2)
 
@@ -532,7 +536,8 @@ def _get_gravity_array(internal_mask, a, b, c, x, y, z, density):
 
     # call functions to produce g values, external and internal
     g_ext_x, g_ext_y, g_ext_z = func(
-        x[~internal_mask], y[~internal_mask], z[~internal_mask], a, b, c, density
+        x[~internal_mask], y[~internal_mask], z[~internal_mask],
+        a, b, c, density
     )
     g_int_x, g_int_y, g_int_z = _get_internal_g(
         x[internal_mask], y[internal_mask], z[internal_mask], a, b, c, density

harmonica/_forward/ellipsoid_magnetics.py
@@ -8,7 +8,7 @@ from scipy.constants import mu_0
 from scipy.special import ellipeinc, ellipkinc
 
 from .ellipsoid_gravity import _get_abc
-from .utils_ellipsoids import _calculate_lambda, _get_v_as_Euler
+from .utils_ellipsoids import _calculate_lambda, _get_v_as_euler
 
 
 # internal field N matrix functions
@@ -119,10 +119,11 @@ def ellipsoid_magnetics(coordinates, ellipsoids, k, h0, field="b"):
 
         # preserve ellipsoid shape, translate origin of ellipsoid
         cast = np.broadcast(e, n, u)
-        obs_points = np.vstack(((e - ox).ravel(), (n - oy).ravel(), (u - oz).ravel()))
+        obs_points = np.vstack(((e - ox).ravel(), (n - oy).ravel(),
+                                (u - oz).ravel()))
 
         # get observation points, rotate them
-        r = _get_v_as_Euler(yaw, pitch, roll)
+        r = _get_v_as_euler(yaw, pitch, roll)
         rotated_points = r.T @ obs_points
         x, y, z = tuple(c.reshape(cast.shape) for c in rotated_points)
 
@@ -189,8 +190,10 @@ def _depol_triaxial_int(a, b, c):
     coeff = (a * b * c) / (np.sqrt(a**2 - c**2) * (a**2 - b**2))
 
     nxx = coeff * (ellipkinc(phi, k) - ellipeinc(phi, k))
-    nyy = -nxx + coeff * ellipeinc(phi, k) - c**2 / (b**2 - c**2)
-    nzz = -coeff * ellipeinc(phi, k) + b**2 / (b**2 - c**2)
+    nyy = -nxx + ((a * b * c) / (np.sqrt(a**2 - c**2) * (b**2 - c**2))) \
+        * ellipeinc(phi, k) - c**2 / (b**2 - c**2)
+    nzz = -((a * b * c) / (np.sqrt(a**2 - c**2) * (b**2 - c**2))) \
+        * ellipeinc(phi, k) + b**2 / (b**2 - c**2)
 
     return nxx, nyy, nzz
 
@@ -212,7 +215,8 @@ def _depol_prolate_int(a, b, c):
 
     """
     m = a / b
-    nxx = 1 / (m**2 - 1) * ((m / np.sqrt(m**2 - 1)) * np.log(m + np.sqrt(m**2 - 1)) - 1)
+    nxx = 1 / (m**2 - 1) * ((m / np.sqrt(m**2 - 1)) *
+                            np.log(m + np.sqrt(m**2 - 1)) - 1)
     nyy = nzz = 0.5 * (1 - nxx)
 
     return nxx, nyy, nzz
@@ -394,7 +398,8 @@ def _get_g_values_magnetics(a, b, c, lmbda):
             ((a**2 - b**2) * (a**2 + lmbda) ** 0.5) / (b**2 + lmbda)
         ) - (
             np.log(
-                ((a**2 - b**2) ** 0.5 + (a**2 + lmbda) ** 0.5) / (b**2 + lmbda) ** 0.5
+                ((a**2 - b**2) ** 0.5 + (a**2 + lmbda) ** 0.5) /
+                (b**2 + lmbda) ** 0.5
             )
         )
         gvals_x, gvals_y, gvals_z = g1, g2, g2
@@ -464,6 +469,7 @@ def _construct_n_matrix_external(x, y, z, a, b, c, lmbda):
                     derivs_lmbda[i] * h_vals[i] * r[i] + gvals[i]
                 )
             else:
-                n[i][j] = (-a * b * c / 2) * (derivs_lmbda[i] * h_vals[j] * r[j])
+                n[i][j] = (-a * b * c / 2) * (derivs_lmbda[i] *
+                                              h_vals[j] * r[j])
 
     return n

harmonica/_forward/utils_ellipsoids.py
@@ -30,7 +30,8 @@ def _calculate_lambda(x, y, z, a, b, c):
         The computed value(s) of the lambda parameter.
 
     """
-    if not (np.any(np.abs(x) >= a) or np.any(np.abs(y) >= b) or np.any(np.abs(z) >= c)):
+    if not (np.any(np.abs(x) >= a) or np.any(np.abs(y) >= b) or
+            np.any(np.abs(z) >= c)):
         raise ValueError(
             "Arrays x, y, z should contain points which lie outside"
             " of the surface defined by a, b, c"
@@ -69,7 +70,7 @@ def _calculate_lambda(x, y, z, a, b, c):
     return lmbda
 
 
-def _get_v_as_Euler(yaw, pitch, roll):
+def _get_v_as_euler(yaw, pitch, roll):
     """
     Generate a rotation matrix (V) from Tait-Bryan angles: yaw, pitch,
     and roll.
@@ -151,7 +152,8 @@ def _global_to_local(northing, easting, extra_coords, depth, v):
     # calculate local_coords for each x, y, z point
     for i in range(len(local_coords)):
         local_coords[i] = (
-            northing * v[i][0] + easting * v[i][1] - (depth - extra_coords) * v[i][2]
+            northing * v[i][0] + easting * v[i][1] - (depth -
+                                                      extra_coords) * v[i][2]
         )
 
     return local_coords