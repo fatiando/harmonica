319caf6401c3617a9a61639d9d25a0ddd082fc64
Santiago Soler <santisoler@fastmail.com>
2025-09-03 14:21:41 -0700

    Improve docstrings in ellipsoid_magnetics.py

--- harmonica/_forward/ellipsoid_magnetics.py
+++ harmonica/_forward/ellipsoid_magnetics.py
@@ -5,8 +5,7 @@
 # This code is part of the Fatiando a Terra project (https://www.fatiando.org)
 #
 """
-Forward modelling for the magnetic field anomaly produced by ellipsoidal
-bodies.
+Forward modelling magnetic fields produced by ellipsoidal bodies.
 """
 
 from collections.abc import Iterable
@@ -31,71 +30,51 @@ def ellipsoid_magnetics(
     field="b",
 ):
     """
-    Produces the components for the magnetic field components (be, bn, bu):
+    Forward model magnetic fields of ellipsoids.
 
-        - Unpacks ellipsoid instance parameters (a, b, c, yaw, pitch, roll,
-          origin)
-        - Constructs Euler rotation matrix
-        - Rotates observation points (e, n, u) into local ellipsoid system
-          (x, y, z)
-        - constructs susceptability matrix and depolarisation matrix for
-          internal
-          and external points of observation
-        - Calculates the magentic field due to the magnetised ellipsoid (H())
-        - Converts this to magentic induction (B()) in nT.
+    Compute the magnetic field components for an ellipsoidal body at specified
+    observation points.
 
     Parameters
     ----------
-    coordinates: tuple of easting (e), northing (n), upward (u) points
-        e : ndarray
-            Easting coordinates, in the form:
-                - A scalar value (float or int)
-                - A 1D array of shape (N,)
-                - A 2D array (meshgrid) of shape (M, N)
-
-        n : ndarray
-            Northing coordinates, same shape and rules as 'e'.
-
-        u : ndarray
-            Upward coordinates, e.g. the surface height desired to compute the
-            gravity value. Same shape and rules as 'e'.
-
-    ellipsoid* : value, or list of values
-        instance(s) of TriaxialEllipsoid, ProlateEllipsoid,
-                 OblateEllipsoid
-        Geometric description of the ellipsoid:
-            - Semiaxes : a, b, c**
-            - Orientation : yaw, pitch, roll**
-            - Origin : centre point (x, y, z)
-
-    susceptibility : list of floats or arrays
-        Susceptibilty value. A single value or list of single values assumes
-        isotropy in the body/bodies. An array or list of arrays should be a 3x3
-        matrix with the given susceptibilty components, suggesting an
-        anisotropic susceptibility.
-
-    external_field : ndarray
+    coordinates : list of arrays
+        List of arrays containing the ``easting``, ``northing`` and ``upward``
+        coordinates of the computation points defined on a Cartesian coordinate
+        system. All coordinates should be in meters.
+    ellipsoid : ellipsoid or list of ellipsoids
+        Ellipsoidal body represented by an instance of
+        :class:`harmonica.TriaxialEllipsoid`, :class:`harmonica.ProlateEllipsoid`, or
+        :class:`harmonica.OblateEllipsoid`, or a list of them.
+    susceptibility : float, (3, 3) array, list of floats, or list of (3, 3) arrays
+        Magnetic susceptibility of the ellipsoid.
+        Pass a float (for a single ellipsoid), or a list of floats (one for each
+        ellipsoid) for isotropic magnetic susceptibility.
+        Pass a (3, 3) array (for a single ellipsoid), or a list of (3, 3)
+        arrays (one for each ellipsoid) as susceptibility tensors, to forward model
+        anisotropic susceptibilities.
+    external_field : (3) array
         The uniform magnetic field (B) as and array with values of
         (magnitude, inclination, declination). The magnitude should be in nT,
         and the angles in degrees.
-
-    remnant_mag:  (optional) array
-        Remnent magnetisation vector of the body. Default is None.
-
-    field : (optional) str, one of either "e", "n", "u".
-        if no input is given, the function will return all three components of
-        magentic induction.
+    remnant_mag : (3) array or list of (3) arrays, optional
+        Remnent magnetisation vector of the ellipsoid, whose components must be in the
+        following order: `magnetizatio_e`, `magnetization_n`, `magnetization_u`.
+        Pass a list of (3) arrays for multiple ellipsoids.
+        If None, no remanent magnetization will be assigned to the ellipsoids.
+        Default is None.
+    field : {"b", "e", "n", "u"}, optional
+        Desired field that want to be computed.
+        If "e", "n", or "u" the function will return the easting, northing or upward
+        magnetic component, respectively.
+        If "b", the function will return a tuple with the three magnetic field
+        components.
+        Default to "b".
 
     Returns
     -------
-    be: ndarray
-        Easting component of the magnetic field.
-
-    bn ndarray
-        Northing component of the magnetic field.
-
-    bu: ndarray
-        Upward component of the magnetic field.
+    be, bn, bu: arrays
+        Easting, northing and upward magnetic field components.
+        Or a single one if ``field`` is "e", "n" or "u".
 
     References
     ----------
@@ -207,8 +186,10 @@ def ellipsoid_magnetics(
 
 def _get_magnetisation_with_rem(a, b, c, k, h0, mr):
     r"""
-    Get the magnetization vector from the ellipsoid parameters and the rotated
-    external field.
+    Get magnetization vector for an ellipsoid.
+
+    Get the magnetization vector from the ellipsoid parameters and the rotated external
+    field.
 
     Parameters
     ----------
@@ -221,23 +202,21 @@ def _get_magnetisation_with_rem(a, b, c, k, h0, mr):
 
     Returns
     -------
-    m (magentisation): array
+    m (magentisation): (3) array
         The magnetisation vector for the defined body.
 
     Notes
     -----
     Considering an ellipsoid with susceptibility :math:`\chi` (scalar or
-                                                               tensor) in
-    a uniform background field :math:`\mathbf{H}_0`, compute the magnetization
-    vector
-    :math:`\mathbf{M}` of the ellipsoid accounting for demagnetization effects
-    as:
+    tensor) in a uniform background field :math:`\mathbf{H}_0`, and with remanent
+    magnetization :math:`\mathbf{M}_r`, compute the magnetization vector
+    :math:`\mathbf{M}` of the ellipsoid accounting for demagnetization effects as:
 
     .. math::
 
         \mathbf{M} =
-        \chi \[left \mathbf{I} + \mathbf{N}^\text{int} \chi \right]^{-1}
-        \mathbf{H}_0,
+        \[left \mathbf{I} + \chi \mathbf{N}^\text{int} \right]^{-1}
+        \[left \chi \mathbf{H}_0 + \mathbf{M}_r \right],
 
     where :math:`\mathbf{N}^\text{int}` is the internal demagnetization tensor,
     defined as:
@@ -271,7 +250,6 @@ def check_susceptibility(susceptibility):
     -------
     k_matrix: array
         the matrix for k (isotropic or anisotropic)
-
     """
     if isinstance(susceptibility, (int, float)):
         k_matrix = susceptibility * np.identity(3)
@@ -301,8 +279,6 @@ def _depol_triaxial_int(a, b, c):
     -------
     nxx, nyy, nzz : floats
         individual diagonal components of the x, y, z matrix.
-
-
     """
     phi = np.arccos(c / a)
     k = (a**2 - b**2) / (a**2 - c**2)
@@ -317,9 +293,9 @@ def _depol_triaxial_int(a, b, c):
         + ((a * b * c) / (np.sqrt(a**2 - c**2) * (b**2 - c**2))) * ellipeinc(phi, k)
         - c**2 / (b**2 - c**2)
     )
-    nzz = -1 * ((a * b * c) / (np.sqrt(a**2 - c**2) * (b**2 - c**2))) * ellipeinc(
-        phi, k
-    ) + b**2 / (b**2 - c**2)
+    nzz = -1 * (
+        (a * b * c) / (np.sqrt(a**2 - c**2) * (b**2 - c**2))
+    ) * ellipeinc(phi, k) + b**2 / (b**2 - c**2)
 
     np.testing.assert_allclose((nxx + nyy + nzz), 1, rtol=1e-4)
     return nxx, nyy, nzz
@@ -327,7 +303,7 @@ def _depol_triaxial_int(a, b, c):
 
 def _depol_prolate_int(a, b):
     """
-    Calcualte internal depolarisation factors for prolate case.
+    Calculate internal depolarisation factors for prolate case.
 
     Parameters
     ----------
@@ -338,7 +314,6 @@ def _depol_prolate_int(a, b):
     -------
     nxx, nyy, nzz : floats
         individual diagonal components of the x, y, z matrix.
-
     """
     m = a / b
     if not m > 1:
@@ -362,7 +337,7 @@ def _depol_prolate_int(a, b):
 
 def _depol_oblate_int(a, b):
     """
-    Calcualte internal depolarisation factors for oblate case.
+    Calculate internal depolarisation factors for oblate case.
 
     Parameters
     ----------
@@ -373,7 +348,6 @@ def _depol_oblate_int(a, b):
     -------
     nxx, nyy, nzz : floats
         individual diagonal components of the x, y, z matrix.
-
     """
     m = a / b
     if not 0 < m < 1:
@@ -399,7 +373,6 @@ def _construct_n_matrix_internal(a, b, c):
     -------
     N : matrix
         depolarisation matrix (diagonal-only values) for the given ellipsoid.
-
     """
     # only diagonal elements
     # Nii corresponds to the above functions
@@ -423,51 +396,45 @@ def _construct_n_matrix_internal(a, b, c):
 
 def _get_h_values(a, b, c, lmbda):
     """
-    Get the h values for the N matrix. Each point has its own h value and hence
-    external N matrix.
+    Get the h values for the N matrix.
+
+    Each point has its own h value and hence external N matrix.
 
     Parameters
     ----------
     a, b, c : floats
         Semiaxis lengths of the given ellipsoid.
-
     lmbda : float
-        the given lmbda value for the point we are considering with this
+        The given lmbda value for the point we are considering with this
         matrix.
 
     Returns
     -------
     h : float
-        the h value for the given point.
-
+        The h value for the given point.
     """
     axes = np.array([a, b, c])
     r = np.sqrt(np.prod(axes**2 + lmbda))
-
     return -1 / ((axes**2 + lmbda) * r)
 
 
 def _spatial_deriv_lambda(x, y, z, a, b, c, lmbda):
     """
-    Get the spatial derivative of lambda with respect to the x,y,z.
+    Get the spatial derivative of lambda with respect to x, y, and z.
 
     Parameters
     ----------
     x, y, z : floats
         A singular observation point in the local coordinate system.
-
     a, b, c : floats
         Semiaxis lengths of the given ellipsoid.
-
     lmbda : float
-        the given lmbda value for the point we are considering with this
-        matrix.
-
+        The given lmbda value for the point we are considering with this matrix.
 
     Returns
     -------
-    vals : array of x, y, z components
-        The spatial derivative for the given point.
+    derivatives : array of x, y, z components
+        The spatial derivatives of lambda for the given point.
 
     """
     # explicitly state:
@@ -493,14 +460,11 @@ def _construct_n_matrix_external(x, y, z, a, b, c, lmbda):
     ----------
     x, y, z : floats
         A singular observation point in the local coordinate system.
-
     a, b, c : floats
         Semiaxis lengths of the given ellipsoid.
-
     lmbda : float
         the given lmbda value for the point we are considering with this
         matrix.
-
 
     Returns
     -------
