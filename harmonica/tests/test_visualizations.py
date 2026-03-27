d263da44457bb4c43e3d3de69d3e1a18f8b8d95a
Santiago Soler <santisoler@fastmail.com>
2026-03-27 10:05:16 -0700

    Mock pyvista to None to trigger test function
    Mock pyvista as missing to run a test function that used to be skipped.
    Remove pragma comment.

--- harmonica/tests/test_visualizations.py
+++ harmonica/tests/test_visualizations.py
@@ -24,12 +24,11 @@ except ImportError:  # pragma: no cover
     pyvista = None
 
 
-@pytest.mark.skipif(pyvista is not None, reason="pyvista must be missing")
-def test_prism_to_pyvista_missing_pyvista():  # pragma: no cover
+@patch("harmonica.visualization._prism.pyvista", None)
+def test_prism_to_pyvista_missing_pyvista():
     """
     Check error raise after calling prism_to_pyvista when pyvista is missing.
     """
-    # TODO: this function is not being covered, check why, or think about removing it.
     prism = [0, 1, 0, 1, 0, 1]
     with pytest.raises(ImportError) as exception:
         prism_to_pyvista(prism)
