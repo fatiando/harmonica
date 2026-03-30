3d15d48e802df39ddddcbc4628e59d08f88a4169
Leonardo Uieda <leo@uieda.com>
2026-03-30 17:44:27 -0300

    Require Boule>=0.6 in code and documentation (#657)
    This release of Boule made backwards incompatible changes. Since it came
    out before the Harmonica release, we should adapt and require the newest
    version. Take this chance to unpin the Verde and scikit-learn versions
    from the documentation building environment.

--- doc/gallery_src/equivalent_sources/spherical.py
+++ doc/gallery_src/equivalent_sources/spherical.py
@@ -43,12 +43,12 @@ blocked_mean = vd.BlockReduce(np.mean, spacing=0.2, drop_coords=False)
 
 # Compute gravity disturbance by removing the gravity of normal Earth
 ellipsoid = bl.WGS84
-gamma = ellipsoid.normal_gravity(latitude, height=elevation)
+gamma = ellipsoid.normal_gravity((longitude, latitude, elevation))
 gravity_disturbance = gravity_data - gamma
 
 # Convert data coordinates from geodetic (longitude, latitude, height) to
 # spherical (longitude, spherical_latitude, radius).
-coordinates = ellipsoid.geodetic_to_spherical(longitude, latitude, elevation)
+coordinates = ellipsoid.geodetic_to_spherical((longitude, latitude, elevation))
 
 # Create the equivalent sources
 eqs = hm.EquivalentSourcesSph(damping=1e-3, relative_depth=10000)
