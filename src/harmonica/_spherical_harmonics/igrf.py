925c5294768ff1d34e95d5665f49a6a173787510
Santiago Soler <santisoler@fastmail.com>
2026-09-11 14:26:31 -0700

    Rename `start_of_the_year` variable to `start_of_epoch`
    Co-authored-by: Leonardo Uieda <leo@uieda.com>

--- src/harmonica/_spherical_harmonics/igrf.py
+++ src/harmonica/_spherical_harmonics/igrf.py
@@ -120,14 +120,14 @@ def interpolate_coefficients(date, max_degree, years, g, h, g_sv, h_sv):
         raise ValueError(message)
     # Find out which epoch is the last compatible one.
     index = int((date.year - years[0]) // 5)
-    start_of_the_year = datetime.datetime(
+    start_of_epoch = datetime.datetime(
         year=years[index],
         month=1,
         day=1,
         # Specify start of the year in UTC time if date has timezone information
         tzinfo=datetime.timezone.utc if date.tzinfo is not None else None,
     )
-    seconds_since_epoch = (date - start_of_the_year).total_seconds()
+    seconds_since_epoch = (date - start_of_epoch).total_seconds()
     epoch = (
         datetime.datetime(year=years[index] + 5, month=1, day=1)  # ruff: ignore[DTZ001]
         - datetime.datetime(year=years[index], month=1, day=1)  # ruff: ignore[DTZ001]
