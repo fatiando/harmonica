8a5c01522c97464e97fa3a916780bc42ba4f91fb
Santiago Soler <santisoler@fastmail.com>
2026-09-11 14:55:37 -0700

    Properly reference `hm.filters.fft` function in docs
    Co-authored-by: Leonardo Uieda <leo@uieda.com>

--- src/harmonica/filters/_fft.py
+++ src/harmonica/filters/_fft.py
@@ -130,7 +130,7 @@ def ifft(fft_grid, *, prefix="freq_"):
     .. important::
 
         Assumes that the ``fft_grid`` is *shifted*: it was passed to
-        :func:`numpy.fft.fftshift`. The outputs of the ``fft`` function satisfy this
+        :func:`numpy.fft.fftshift`. The outputs of the :func:`harmonica.filters.fft` function satisfy this
         condition.
 
     Parameters
